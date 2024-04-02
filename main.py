7ff49e53387cd18488746b961a28e77e757e6772
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-02 16:43:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": [
-                "Service": "es.amazonaws.com",
-				"AWS": "arn:aws:iam::420737321821:role/CUSPFE-ias-test-vsl-docinsight-opensearch-migration"
-				],
-            "Action": "sts:AssumeRole"
-        }
-    ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "es.amazonaws.com",
+        "AWS": "arn:aws:iam::420737321821:role/CUSPFE-ias-test-vsl-docinsight-opensearch-migration"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
 }
