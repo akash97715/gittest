e389467580a6424758482ed3fe700acf65f70953
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-03 15:17:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 {
-    "Version": "2012-10-17",
-    "Id": "Policy1568001010746",
-    "Statement": [{
-        "Sid": "Stmt1568000712531",
-        "Effect": "Allow",
-        "Principal": {
-            "AWS": ["arn:aws:iam::808950231959:role/CUSPFE-ias-prod-vsl-doc-os-migration", "arn:aws:iam::808950231959:role/CUSPFE-ias-prod-vsl-doc-os-migration-role"]
-        },
-        "Action": "s3:*",
-        "Resource": "arn:aws:s3:::test-docinsight-opensearch-migration"
-    }, {
-        "Sid": "Stmt1568001007239",
-        "Effect": "Allow",
-        "Principal": {
-            "AWS": ["arn:aws:iam::808950231959:role/CUSPFE-ias-prod-vsl-doc-os-migration", "arn:aws:iam::808950231959:role/CUSPFE-ias-prod-vsl-doc-os-migration-role"]
-        },
-        "Action": "s3:*",
-        "Resource": "arn:aws:s3:::test-docinsight-opensearch-migration/*"
-    }]
+  "Version": "2012-10-17",
+  "Id": "Policy1568001010746",
+  "Statement": [
+    {
+      "Sid": "Stmt1568000712531",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": ["arn:aws:iam::808950231959:role/CUSPFE-ias-prod-vsl-doc-os-migration", "arn:aws:iam::808950231959:role/CUSPFE-ias-prod-vsl-doc-os-migration-role"]
+      },
+      "Action": "s3:*",
+      "Resource": "arn:aws:s3:::test-docinsight-opensearch-migration"
+    },
+    {
+      "Sid": "Stmt1568001007239",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": ["arn:aws:iam::808950231959:role/CUSPFE-ias-prod-vsl-doc-os-migration", "arn:aws:iam::808950231959:role/CUSPFE-ias-prod-vsl-doc-os-migration-role"]
+      },
+      "Action": "s3:*",
+      "Resource": "arn:aws:s3:::test-docinsight-opensearch-migration/*"
+    },
+    {
+      "Sid": "DelegateS3Access",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::808950231959:role/PFE-RAPID-CUSTOMER-ADMIN-SSO"
+      },
+      "Action": "s3:*",
+      "Resource": [
+        "arn:aws:s3:::test-docinsight-opensearch-migration",
+        "arn:aws:s3:::test-docinsight-opensearch-migration/*"
+      ]
+    }
+  ]
 }
- 
- 
-{
- 
-            "Sid": "DelegateS3Access",
- 
-            "Effect": "Allow",
- 
-            "Principal": {
- 
-                "AWS": "arn:aws:iam::808950231959:role/PFE-RAPID-CUSTOMER-ADMIN-SSO"
- 
-            },
