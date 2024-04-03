a831ea69c3446166d18525c74a7f1d5790c41016
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-03 11:19:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "es.amazonaws.com",
-        "AWS": "arn:aws:iam::420737321821:role/CUSPFE-ias-test-vsl-docinsight-opensearch-migration"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
+    def __init__(self, template=None):
+        if template is None:
+            current_script_dir = os.path.dirname(os.path.abspath(__file__))
+           
+            req_path=os.path.join(current_script_dir,"test-utility.pptx")
+           
+            self.presentation = Presentation(req_path)
+           
+ 
+        else:
+            self.presentation = Presentation()
+        self.presentation.slide_width = Inches(13.33)  # 16:9 aspect ratio width
+        self.presentation.slide_height = Inches(7.5)
+        self.light_gray = RGBColor(217, 217, 217)
