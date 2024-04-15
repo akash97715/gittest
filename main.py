fbaf95593a94f2640d344a3b4b4e570593a2fada
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-15 17:16:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ class Document:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
-# Assuming the initialization of S3 client and bucket name are set up elsewhere
 s3 = boto3.client('s3')
 bucket_name = 'your-bucket-name'
 
-documents = [Document(**(json.loads(obj["Body"].read().decode('utf-8')) if isinstance(obj["Body"].read().decode('utf-8'), str) else {'content': obj["Body"].read().decode('utf-8')})) for key in keys for obj in [s3.get_object(Bucket=bucket_name, Key=key)]]
+def parse_content(key):
+    try:
+        # Fetch the object from S3
+        response = s3.get_object(Bucket=bucket_name, Key=key)
+        # Read the content of the object and attempt to parse as JSON
+        return json.loads(response['Body'].read().decode('utf-8'))
+    except json.JSONDecodeError:
+        # If JSON decoding fails, return as plain string in a dict
+        return {'content': response['Body'].read().decode('utf-8')}
+
+# List comprehension to process each key
+documents = [Document(**parse_content(key)) for key in keys]
