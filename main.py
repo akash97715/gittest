9c5f787ade6d300b0a3ac11f41b79386effe665f
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-18 16:07:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-extractor = Textractor(profile_name="default")
-tables_analysis_doc_object = extractor.start_document_analysis(f"Docs/{doc_name}",
+layout_analysis_doc_object = extractor.start_document_analysis(file_source="s3://sbx-docinsight-ocr/[II_MANU_01_OUT_01]Odufalu FD MS OUTPUT.pdf",
                                                   s3_upload_path=s3_upload_path,
-                                              features=[TextractFeatures.TABLES])
+                                              features=[TextractFeatures.LAYOUT])
+layout_analysis_doc_object.layouts
