3cb2067d53930573298e941206d478df0c929acc
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-22 15:59:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,136 @@
-document_text_layout = []
- 
-# for every page extract text layouts with relevant metadata plus full page text
-for page in layout_analysis_doc_object.pages:
- 
-    page_full_text = page.text  # will return also texted included in fugures and tables
- 
-    text_layouts_extracted = []
- 
-    for index, layout in enumerate(page.layouts):
-        if layout.layout_type != 'LAYOUT_FIGURE' and layout.layout_type != 'LAYOUT_TABLE':
-            layout_name = f'page_{layout.page}_layout_{index}'
-            layout_id = layout.id
-            layout_reading_order = layout.reading_order
-            layout_type = layout.layout_type
-            layout_text = layout.text
-            layout_conf = layout.confidence
-            layout_width = layout.width
-            layout_height = layout.height
-            layout_x = layout.x
-            layout_y = layout.y
- 
-            layout_data = {
-                    "layout_id": layout_id,
-                    "layout_name": layout_name,
-                    "layout_reading_order": layout_reading_order,
-                    "layout_type": layout_type,
-                    "layout_text": layout_text,
-                    "layout_confidence": layout_conf,
-                    "layout_width": layout_width,
-                    'layout_height': layout_height,
-                    'layout_x': layout_x,
-                    'layout_y': layout_y,
-                    'layout_page': layout.page,
+import os
+import logging
+
+class DocumentAnalyzer:
+    def __init__(self, extractor, file_source, doc_name, s3_upload_path):
+        self.extractor = extractor
+        self.file_source = file_source
+        self.doc_name = doc_name
+        self.s3_upload_path = s3_upload_path
+        self.extracted_figures = []
+        self.extracted_tables = []
+        self.document_text_layouts = []
+
+    def start_analysis(self, features):
+        try:
+            return self.extractor.start_document_analysis(file_source=self.file_source,
+                                                          s3_upload_path=self.s3_upload_path,
+                                                          features=features)
+        except Exception as e:
+            logging.error("Failed to start document analysis: %s", e)
+            raise RuntimeError("Document analysis could not be started.") from e
+
+    def extract_figures(self):
+        document = self.start_analysis([TextractFeatures.LAYOUT, TextractFeatures.TABLES])
+        for page in document.pages:
+            self._process_page_for_figures(page)
+
+    def _process_page_for_figures(self, page):
+        figures_in_page = [layout for layout in page.layouts if layout.layout_type == 'LAYOUT_FIGURE']
+        if figures_in_page:
+            for index, figure in enumerate(figures_in_page):
+                cropped_img = self._crop_image(page.image, figure.bbox, page.image.size)
+                figure_path = self._save_cropped_image(cropped_img, "figures", f"page_{page.page_num}_figure_{index + 1}.png")
+                figure_info = self._gather_figure_metadata(figure, figure_path, page.page_num)
+                self.extracted_figures.append(figure_info)
+
+    def extract_tables(self):
+        document = self.start_analysis([TextractFeatures.TABLES])
+        for page in document.pages:
+            self._process_page_for_tables(page)
+
+    def _process_page_for_tables(self, page):
+        if page.tables:
+            for index, table in enumerate(page.tables):
+                cropped_img = self._crop_image(page.image, table.bbox, page.image.size)
+                table_img_path = self._save_cropped_image(cropped_img, "tables/img", f"page_{page.page_num}_table_{index + 1}.png")
+                table_data = self._gather_table_metadata(table, table_img_path, page.page_num)
+                self.extracted_tables.append(table_data)
+
+    def extract_raw_text(self):
+        document = self.start_analysis([TextractFeatures.LAYOUT])
+        for page in document.pages:
+            self._process_page_for_text(page)
+
+    def _process_page_for_text(self, page):
+        page_full_text = page.text
+        text_layouts_extracted = []
+
+        for index, layout in enumerate(page.layouts):
+            if layout.layout_type not in ['LAYOUT_FIGURE', 'LAYOUT_TABLE']:
+                layout_data = {
+                    "layout_id": layout.id,
+                    "layout_name": f'page_{layout.page}_layout_{index}',
+                    "layout_reading_order": layout.reading_order,
+                    "layout_type": layout.layout_type,
+                    "layout_text": layout.text,
+                    "layout_confidence": layout.confidence,
+                    "layout_width": layout.width,
+                    "layout_height": layout.height,
+                    "layout_x": layout.x,
+                    "layout_y": layout.y,
+                    "layout_page": layout.page,
                 }
- 
-        text_layouts_extracted.append(layout_data)
- 
-    page_text_layout = {
-        "document_name": doc_name,
-        "page_num": page.page_num,
-        "page_full_text": page_full_text,
-        "text_layout_data": text_layouts_extracted
-    }
- 
-    document_text_layout.append(page_text_layout)
+                text_layouts_extracted.append(layout_data)
+
+        page_text_layout = {
+            "document_name": self.doc_name,
+            "page_num": page.page_num,
+            "page_full_text": page_full_text,
+            "text_layout_data": text_layouts_extracted
+        }
+
+        self.document_text_layouts.append(page_text_layout)
+
+    def _crop_image(self, image, bbox, size):
+        x, y, width, height = bbox.x * size[0], bbox.y * size[1], bbox.width * size[0], bbox.height * size[1]
+        return image.crop((x, y, x + width, y + height))
+
+    def _save_cropped_image(self, image, subdir, filename):
+        path = os.path.join(self.doc_name, subdir)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        image_path = os.path.join(path, filename)
+        image.save(image_path)
+        return image_path
+
+    def _gather_figure_metadata(self, figure, figure_path, page_num):
+        return {
+            "document_name": self.doc_name,
+            "figure_id": figure.id,
+            "figure_name": os.path.basename(figure_path),
+            "figure_page": page_num,
+            "figure_confidence": figure.confidence,
+            "figure_bbox": figure.bbox,
+            "figure_height": figure.height,
+            "figure_width": figure.width,
+            "figure_x": figure.x,
+            "figure_y": figure.y,
+            "figure_path": figure_path
+        }
+
+    def _gather_table_metadata(self, table, table_img_path, page_num):
+        column_headers = [cell.text for cell in table.table_cells if cell.is_column_header]
+        footers = [footer.text for footer in table.footers]
+        return {
+            "document_name": self.doc_name,
+            "table_id": table.id,
+            "table_name": os.path.basename(table_img_path),
+            "table_bbox": table.bbox,
+            "table_column_count": table.column_count,
+            "table_title": getattr(table.title, 'text', None),
+            "column_headers": column_headers,
+            "footers": footers,
+            "table_height": table.height,
+            "table_metadata": table.metadata,
+            "table_page_number": page_num,
+            "table_page_id": table.page_id,
+            "table_confidence": table.raw_object['Confidence'],
+            "table_row_count": table.row_count,
+            "table_img_path": table_img_path,
+            "table_html": table.to_html(),
+            "table_markdown": table.to_markdown(),
+            "table_width": table.width,
+            "table_x": table.x,
+            "table_y": table.y
+        }
