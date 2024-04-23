a5767be3d96781224231224da49f88d012a2b2ca
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-23 11:43:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
-def mset(self, key_value_pairs: Sequence[Tuple[K, V]]) -> None:
+from langchain_core.stores import BaseStore
+from typing import Iterator, Sequence, Tuple, TypeVar,Optional
+from langchain_core.documents import  Document
+from typing import Dict, List, ClassVar, Collection
+import json
+K = TypeVar("K")
+V = TypeVar("V")
+ 
+import boto3
+ 
+class S3DocStore(BaseStore):
+    def __init__(self, bucket_name):
+        self.s3 = boto3.client('s3')
+        self.bucket = bucket_name
+ 
+    def mset(self, key_value_pairs: Sequence[Tuple[K, V]])->None:
         for key, document in key_value_pairs:
-            print("the key is", key)
-            self.s3.put_object(
+            print("KV is",key_value_pairs)
+            print("the key is",key,document)
+            if isinstance(document,Document):
+                self.s3.put_object(
                 Body=json.dumps(dict(document)),
                 Bucket=self.bucket,
                 Key=f"{key}",
             )
+            else:
+                self.s3.put_object(
+                Body=json.dumps(document),
+                Bucket=self.bucket,
+                Key=f"{key}",
+            )
+ 
+    def mget(self, keys: Sequence[K])-> List[Optional[V]]:
+        print("the retreived keys are",keys)
+        return  [json.loads(self.s3.get_object(Bucket=self.bucket, Key=f"{key}")["Body"].read()) for key in keys]
+   
+    def mdelete(self, keys: Sequence) -> None:
+        for key in keys:
+            self.s3.delete_object(Bucket=self.bucket, key=key)
+ 
+    def yield_keys(self, *, prefix: str | None = None) -> Iterator | Iterator[str]:
+        pass
