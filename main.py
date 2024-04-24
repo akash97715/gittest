273131676676ab385c52ad0a056d8e42ee3366ac
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-24 16:45:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,1 @@
-self.client_id
-        self.index_name
-        img_ids = ["client_id_"+"index_name"+str(uuid.uuid4()) for _ in self.extracted_figures]
+img_ids = ["{}_{}{}".format(self.client_id, self.index_name, uuid.uuid4()) for _ in self.extracted_figures]
