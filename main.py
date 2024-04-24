0888f3c3afa611e886ec3ad71e65c7d35f5dd0b6
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-25 03:01:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-img_ids = ["{}_{}{}".format(self.client_id, self.index_name, uuid.uuid4()) for _ in self.extracted_figures]
+page_content=f"{self.filename} {item.get('table_page_number', '')} {item.get('table_title', '')} {' '.join(item.get('footers', []))}".strip(),
