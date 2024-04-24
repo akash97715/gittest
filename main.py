d9fec68c4b64c0d43508a7a5db5b29ea8d2408db
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-24 08:12:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-if isinstance(obj, Document) or type(obj) !='str'
+(docinsightenv) D:\opensearchindexfix\vessel-services\docinsight>alembic revision --autogenerate -m "adding extra metadata column"
+INFO  [alembic.runtime.migration] Context impl PostgresqlImpl.
+INFO  [alembic.runtime.migration] Will assume transactional DDL.
+ERROR [alembic.util.messaging] Can't proceed with --autogenerate option; environment script D:\opensearchindexfix\vessel-services\docinsight\alembic\env.py does not provide a MetaData object or sequence of objects to the context.
+FAILED: Can't proceed with --autogenerate option; environment script D:\opensearchindexfix\vessel-services\docinsight\alembic\env.py does not provide a MetaData object or sequence of objects to the context.
