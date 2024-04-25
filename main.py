04ade51b342bc27cfcc922d3d8d3342a942a4257
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-25 14:53:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-[2:04 PM] Deep, Akash (External)
-"""
-    Initializes the customer credentials needed to make calls to Textract using boto3 package internally.
+# don't know right image in the private registry. replace this with right image
+ARG BASE_IMAGE=420737321821.dkr.ecr.us-east-1.amazonaws.com/core-vsl-python3.11-slim
+FROM $BASE_IMAGE
+#ARG BASE_IMAGE=python:3.10-slim
  
-    :param profile_name: Customer's profile name as set in the ~/.aws/config file. This profile typically contains this format.
-                                :code:`[default]
-                                region = us-west-2
-                                output=json`
-    :type profile_name: str
-    :param region_name: If AWSCLI isn't setup, the user can pass region to let boto3 pick up credentials from the system.
-    :param region_name: str
-    :type profile_name: str, optional
-    :param kms_key_id: Customer's AWS KMS key (cryptographic key)
-    :type kms_key_id: str, optional
-[2:05 PM] Deep, Akash (External)
-Textractor(profile_name="default")
+# FROM python:3.9.17
+ 
+ 
+# Create a new user and group
+# RUN addgroup --system testgroup && adduser --system --group testuser
+ 
+# Set the working directory
+WORKDIR /backend
+ 
+ 
+ 
+# Copy only the requirements file to avoid rebuilding dependencies
+COPY ./requirements.txt /backend/
+ 
+# Install any needed packages specified in requirements.txt using apk and pip
+RUN pip install --no-cache-dir --upgrade pip
+# keeping it separate as the req file might need update and also to cache the previous step
+RUN pip install --no-cache-dir -r requirements.txt
+RUN python -m spacy download en_core_web_sm
+ 
+## Copy the application code into the container
+COPY . /backend/
+RUN pip install -e .
+ 
+# Switch to non-root user
+# USER testuser
+#ARG URL_PATTERN=vox
+#ENV URL_PATTERN=$URL_PATTERN
+ 
+# Expose port 80 to the Docker host, so we can access the application outside the container
+EXPOSE 8000
+ 
+#ENTRYPOINT ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--workers", "2", "--proxy-headers", "--forwarded-allow-ips", "*", "--access-log", "--log-level", "info"]
+# RUN set PYTHONPATH=$PWD
+CMD ["python", "/backend/main.py"]
