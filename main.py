b3855ab2c57c2971baf1f49261bbfb5c4cd1232b
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-25 14:57:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
-# Use the specified base image from the private registry
-ARG BASE_IMAGE=420737321821.dkr.ecr.us-east-1.amazonaws.com/core-vsl-python3.11-slim
-FROM $BASE_IMAGE
-
-# Set the working directory inside the container
-WORKDIR /backend
-
-# Install system dependencies
-# Assuming Debian-based image as we are using apt-get
-RUN apt-get update && apt-get install -y \
-    poppler-utils \    # Install poppler-utils for PDF processing
-    && rm -rf /var/lib/apt/lists/*  # Clean up to reduce image size
-
-# Copy the requirements file first to leverage Docker cache
-COPY ./requirements.txt /backend/
-
-# Install Python dependencies from requirements file
-RUN pip install --no-cache-dir --upgrade pip \
-    && pip install --no-cache-dir -r requirements.txt \
-    && python -m spacy download en_core_web_sm
-
-# Copy the rest of the application code into the container
-COPY . /backend/
-
-# Install the application (editable mode assumes setup.py is present)
-RUN pip install -e .
-
-# Optional: Uncomment and modify if you need to use a non-root user
-# RUN addgroup --system testgroup && adduser --system --group testuser
-# USER testuser
-
-# Expose port 8000 for the application
+FROM  420737321821.dkr.ecr.us-east-1.amazonaws.com/core-vsl-python3.11-slim
+ 
+ENV PYTHONUNBUFFERED=1 \
+    PYTHONDONTWRITEBYTECODE=1 \
+    LOG_LEVEL=warning
+ 
+# RUN apt-get update
+# RUN apt-get install --no-install-recommends -y libgl1 poppler-utils libmagic-dev libreoffice tesseract-ocr
+ 
+COPY requirements.txt .
+COPY libzip.tar .
+ 
+RUN python -m pip install --no-cache-dir -r requirements.txt
+RUN python -m pip list
+ 
+WORKDIR /app
+ 
+# Copy the application code into the container
+COPY . /app
+ 
 EXPOSE 8000
-
-# Set the command to run the application
-# Update this command according to how you actually run your application, e.g., using a WSGI server like Gunicorn
-CMD ["python", "/backend/main.py"]
+ 
+ 
+RUN chmod 700 prestart.sh
+ENTRYPOINT ["/bin/bash"]
+CMD ["./prestart.sh"]
