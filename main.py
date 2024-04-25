5a2fef1a7f61c91ca03a2962f598ae4a2b0ccb7d
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-25 14:55:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,37 @@
-# don't know right image in the private registry. replace this with right image
+# Use the specified base image from the private registry
 ARG BASE_IMAGE=420737321821.dkr.ecr.us-east-1.amazonaws.com/core-vsl-python3.11-slim
 FROM $BASE_IMAGE
-#ARG BASE_IMAGE=python:3.10-slim
- 
-# FROM python:3.9.17
- 
- 
-# Create a new user and group
-# RUN addgroup --system testgroup && adduser --system --group testuser
- 
-# Set the working directory
+
+# Set the working directory inside the container
 WORKDIR /backend
- 
- 
- 
-# Copy only the requirements file to avoid rebuilding dependencies
+
+# Install system dependencies
+# Assuming Debian-based image as we are using apt-get
+RUN apt-get update && apt-get install -y \
+    poppler-utils \    # Install poppler-utils for PDF processing
+    && rm -rf /var/lib/apt/lists/*  # Clean up to reduce image size
+
+# Copy the requirements file first to leverage Docker cache
 COPY ./requirements.txt /backend/
- 
-# Install any needed packages specified in requirements.txt using apk and pip
-RUN pip install --no-cache-dir --upgrade pip
-# keeping it separate as the req file might need update and also to cache the previous step
-RUN pip install --no-cache-dir -r requirements.txt
-RUN python -m spacy download en_core_web_sm
- 
-## Copy the application code into the container
+
+# Install Python dependencies from requirements file
+RUN pip install --no-cache-dir --upgrade pip \
+    && pip install --no-cache-dir -r requirements.txt \
+    && python -m spacy download en_core_web_sm
+
+# Copy the rest of the application code into the container
 COPY . /backend/
+
+# Install the application (editable mode assumes setup.py is present)
 RUN pip install -e .
- 
-# Switch to non-root user
+
+# Optional: Uncomment and modify if you need to use a non-root user
+# RUN addgroup --system testgroup && adduser --system --group testuser
 # USER testuser
-#ARG URL_PATTERN=vox
-#ENV URL_PATTERN=$URL_PATTERN
- 
-# Expose port 80 to the Docker host, so we can access the application outside the container
+
+# Expose port 8000 for the application
 EXPOSE 8000
- 
-#ENTRYPOINT ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--workers", "2", "--proxy-headers", "--forwarded-allow-ips", "*", "--access-log", "--log-level", "info"]
-# RUN set PYTHONPATH=$PWD
+
+# Set the command to run the application
+# Update this command according to how you actually run your application, e.g., using a WSGI server like Gunicorn
 CMD ["python", "/backend/main.py"]
