5aa2294089eeb31a8251402cf7effdce59cee1b0
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-25 14:59:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
-FROM  420737321821.dkr.ecr.us-east-1.amazonaws.com/core-vsl-python3.11-slim
- 
+FROM 420737321821.dkr.ecr.us-east-1.amazonaws.com/core-vsl-python3.11-slim
+
 ENV PYTHONUNBUFFERED=1 \
     PYTHONDONTWRITEBYTECODE=1 \
     LOG_LEVEL=warning
- 
-# RUN apt-get update
-# RUN apt-get install --no-install-recommends -y libgl1 poppler-utils libmagic-dev libreoffice tesseract-ocr
- 
+
+# Update package lists and install required packages
+RUN apt-get update \
+    && apt-get install --no-install-recommends -y \
+        libgl1 \
+        poppler-utils \  # Ensure poppler-utils is included for PDF processing
+        libmagic-dev \
+        libreoffice \
+        tesseract-ocr \
+    && rm -rf /var/lib/apt/lists/*  # Clean up to reduce image size
+
 COPY requirements.txt .
 COPY libzip.tar .
- 
+
 RUN python -m pip install --no-cache-dir -r requirements.txt
 RUN python -m pip list
- 
+
 WORKDIR /app
- 
+
 # Copy the application code into the container
 COPY . /app
- 
+
 EXPOSE 8000
- 
- 
+
 RUN chmod 700 prestart.sh
 ENTRYPOINT ["/bin/bash"]
 CMD ["./prestart.sh"]
