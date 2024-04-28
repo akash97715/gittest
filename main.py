f46bf15b84109ce940bae14138a153bf2c14c8ff
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-28 21:45:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-from pydantic import BaseModel, Field
-from typing import List, Optional
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from .controller import ContentFetcher
+from validator.decorators import async_token_validation_and_metering
+from app.langchain.database import get_db
+from fastapi import Request as fastapi_request
 
-class FetchContentRequest(BaseModel):
-    uuids: Optional[List[str]] = Field(
-        default=None,
-        description="A list of UUIDs to fetch content for."
-    )
-    content_type: Optional[str] = Field(
-        default=None,
-        description="Type of content to fetch: 'tables', 'images', or 'both'."
-    )
-    request_id: Optional[str] = Field(
-        default=None,
-        description="Request ID associated with the content type."
-    )
+router = APIRouter()
+
+@router.post(
+    "/fetch-content",
+    status_code=200,
+    tags=["Content Fetcher"],
+    description="This endpoint fetches content from S3 based on direct UUIDs or a combination of content type and request ID, returning the actual content.",
+    response_model=List[Dict]
+)
+@async_token_validation_and_metering()
+async def fetch_content_endpoint(
+    request: fastapi_request,
+    fetch_request: FetchContentRequest,  # Using the Pydantic model to parse and validate input
+    db: Session = Depends(get_db)
+):
+    client_id = request.headers.get("x-agw-client_id")
+    if not client_id:
+        raise HTTPException(
+            status_code=400, detail="Client Id is not sent in headers"
+        )  
+    content_fetcher = ContentFetcher(db)  # Instantiate the ContentFetcher with the database session
+    try:
+        # Using the attributes from the Pydantic model directly
+        content_list = await content_fetcher.fetch_content_from_uuids_or_type(
+            fetch_request.uuids, 
+            fetch_request.content_type, 
+            fetch_request.request_id
+        )
+        return content_list
+    except HTTPException as e:
+        # More sophisticated error handling and logging could be added here
+        raise HTTPException(status_code=e.status_code, detail=e.detail)
