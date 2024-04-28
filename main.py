e702ec51e578b4164c275979cec44d6a72b4247d
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-28 21:38:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-File "D:\docinsightenv\Lib\site-packages\fastapi\routing.py", line 222, in app
-    raise RequestValidationError(
-fastapi.exceptions.RequestValidationError: 1 validation error for Request
-body -> 128
-  Expecting property name enclosed in double quotes: line 3 column 1 (char 128) (type=value_error.jsondecode; msg=Expecting property name enclosed in double quotes; doc={
-   "uuids": ["885ba9df6be84f57b0fcd50b9220a9153612/dbc180e3778df3c74d6908991825eaa3/e5ee084a-8aae-4b14-a6fc-4c05f3b25a18"],
-}; pos=128; lineno=3; colno=1)
-|
+# fetch_doc_content/router.py
+from fastapi import APIRouter, Depends, HTTPException
+from typing import List, Dict, Optional
+from .controller import ContentFetcher
+from validator.decorators import async_token_validation_and_metering
+from app.langchain.database import get_db
+from fastapi import Request as fastapi_request
+from fastapi import APIRouter, Depends, Body, Query, HTTPException
+ 
+ 
+ 
+router = APIRouter()
+ 
+@router.post(
+    "/fetch-content",
+    status_code=200,
+    tags=["Content Fetcher"],
+    description="This endpoint fetches content from S3 based on direct UUIDs or a combination of content type and request ID, returning the actual content.",
+    response_model=List[Dict]
+)
+@async_token_validation_and_metering()
+async def fetch_content_endpoint(
+    request:fastapi_request,
+    uuids: Optional[List[str]] = Body(default=None, description="A list of UUIDs to fetch content for."),
+    content_type: Optional[str] = Query(default=None, description="Type of content to fetch: 'tables', 'images', or 'both'."),
+    request_id: Optional[str] = Query(default=None, description="Request ID associated with the content type."),
+    db=Depends(get_db)
+):
+    client_id = (
+        request.headers.get("x-agw-client_id")
+        if request.headers.get("x-agw-client_id") is not None
+        else None
+    )
+ 
+    if not client_id:
+        raise HTTPException(
+            status_code=400, detail="Client Id is not sent in headers"
+        )  
+    content_fetcher = ContentFetcher(db)  # Instantiate the ContentFetcher with the database session
+    try:
+        content_list = await content_fetcher.fetch_content_from_uuids_or_type(uuids, content_type, request_id)
+        return content_list
+    except HTTPException as e:
+        # You can add more sophisticated error handling and logging here if needed
+        raise HTTPException(status_code=e.status_code, detail=e.detail)
