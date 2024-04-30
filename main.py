be863cd66756a5cf1a19e5787fe176cdfbe3f3e4
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-04-30 12:15:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-        summary_table = [
-            Document(
-                page_content=f"{self.filename} {item.get('figure_page', '')} {item.get('table_title', '')} {' '.join(item.get('footers', []))} {' '.join(item.get('column_headers', []))}".strip(),
-                metadata={
-                    **metadata_dict,
-                    "page_number": item.get("figure_page", ""),
-                    "document_name": self.filename,
-                    "id_key": table_ids[index],
-                    "type":"table",
-                },
-            )
-            for index, item in enumerate(ghg_table)
-        ]
+summary_table = [
+    Document(
+        page_content=f"{self.filename} {item.get('figure_page', '')} {item.get('table_title', '')} {' '.join(item.get('footers', []))} {' '.join(item.get('column_headers', []))} {' ' + item.get('raw_table_html', '') if self.embed_raw_table else ''}".strip(),
+        metadata={
+            **metadata_dict,
+            "page_number": item.get("figure_page", ""),
+            "document_name": self.filename,
+            "id_key": table_ids[index],
+            "type": "table",
+        },
+    )
+    for index, item in enumerate(ghg_table)
+]
