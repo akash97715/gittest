791c4bac1064ec680fc62da7542617b9eafb0860
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-02 15:02:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-import json
-import requests
-
-def lambda_handler(event, context):
-    response = requests.get("https://api.example.com/data")
-    return {
-        'statusCode': 200,
-        'body': json.dumps(response.json())
-    }
-
-
-cd lambda-env\Lib\site-packages
-
-
-Compress-Archive -Path * -DestinationPath C:\path\to\your\project\function.zip -Force
-
-
-cd C:\path\to\your\project
-
-
-Compress-Archive -Path lambda_function.py -Update -DestinationPath function.zip
+PS D:\summarize-lambda\lambda-env\Lib\site-packages> Compress-Archive -Path * -DestinationPath D:\summarize-lambda\function.zip -ForceCompress-Archive -Path * -DestinationPath D:\summarize-lambda\function.zip -Force
+Compress-Archive : Cannot bind parameter because parameter 'Path' is specified more than once. To provide multiple
+values to parameters that can accept multiple values, use the array syntax. For example, "-parameter
+value1,value2,value3".
+At line:1 char:99
++ ... D:\summarize-lambda\function.zip -ForceCompress-Archive -Path * -Dest ...
++                                                             ~~~~~
+    + CategoryInfo          : InvalidArgument: (:) [Compress-Archive], ParameterBindingException
+    + FullyQualifiedErrorId : ParameterAlreadyBound,Compress-Archive
+ 
+PS D:\summarize-lambda\lambda-env\Lib\site-packages>
