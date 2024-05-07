848ef32aa1f786062135085f0f937c3ba4cb92ae
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-07 10:52:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 def _get_images_information(self, image_list, path, image_layout_mapping):
-        img_base64_list = []
-        img_ids = []
-        image_summaries = []
-       
-        # Iterate over the sorted list of files that are relevant
-        for img_file in sorted(image_list):
-            if img_file.endswith(".jpg"):
-                img_path = os.path.join(path, img_file)
-                id_key = "{}/{}/{}".format(self.client_id, self.index_name, uuid.uuid4())
-                base64_image = self._encode_image(img_path)
-               
-                img_base64_list.append(base64_image)
-                img_ids.append(id_key)
+    img_base64_list = []
+    img_ids = []
+    image_summaries = []
 
+    # Iterate over the sorted list of files that are relevant
+    for img_file in sorted(image_list):
+        if img_file.endswith(".jpg"):
+            img_path = os.path.join(path, img_file)
+            id_key = "{}/{}/{}".format(self.client_id, self.index_name, uuid.uuid4())
+            base64_image = self._encode_image(img_path)
 
-  data:image/jpeg;base64,
+            # Add the "data:image/jpeg;base64," prefix to the base64 string
+            img_base64_list.append("data:image/jpeg;base64," + base64_image)
+            img_ids.append(id_key)
