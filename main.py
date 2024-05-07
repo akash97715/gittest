a7eec9e00f3e5cfaec37ee3e4b0dc6b7adc42d73
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-07 20:49:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 GET example_index/_search
 {
-  "size": 10000,  // Adjust based on the maximum expected number of documents
-  "track_total_hits": true,  // This ensures you get the exact count of total matches
   "query": {
-    "match": {
-      "metadata.request_id": "4547d927-df8b-45ea-bb69-f2578f89d5c6"
+    "term": {
+      "metadata.request_id.keyword": {
+        "value": "4547d927-df8b-45ea-bb69-f2578f89d5c6"
+      }
     }
   }
 }
