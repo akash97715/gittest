fb5b5c69d079cb872f9ae3ae195bf63888aa8273
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-09 16:45:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-[4:35 PM] Deep, Akash (External)
-{'document_path': 'c03c7973-8051-4f43-ab6d-850f8651f972', 'table_id': 'd19f0ab2-cb75-44db-9fd4-b99c25b239ce', 'figure_name': 'page_10_table_1_c270b578-64ab-469a-a782-5bf759e3b8d5.jpg', 'table_bbox': x: 0.15217232704162598, y: 0.0721471980214119, width: 0.7117686867713928, height: 0.5322176218032837, 'table_column_count': 6, 'table_title': None, 'column_headers': ['', 'Patients,\n', 'n\n', 'Real-World\n\nProgression-Free\n PAL+AI vs\n', 'Survival\n Al Alone\n', '', 'Subgroup\n', 'PAL+AI\n', 'Al Alone\n', 'Hazard Ratio\n', '(95% CI)\n', ''], 'footers': [], 'table_height': 0.5322176218032837, 'table_metadata': {}, 'figure_page': 10, 'table_page_id': 'f69f2d0b-5e18-44d5-8579-7b496564f622', 'table_confidence': 99.853515625, 'table_row_count': 37, 'table_img_path': 'c03c7973-8051-4f43-ab6d-850f8651f972\\tables/img\\page_10_table_1_c270b578-64ab-469a-a782-5bf759e3b8d5.jpg', 'table_html':
-[4:36 PM] Deep, Akash (External)
-else:
+        if self.advance_table_filter:
+            await self._verify_table_layout(cropped_image, table_info)
+        else:
             print("TABLE INFORMATION===========================================",table_info)
            
             self.extracted_tables.append(table_info)
