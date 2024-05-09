1bfb53d0204ef868abc4c2b3eb2f0e34c5d2a7dd
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-09 16:50:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-        if self.advance_table_filter:
-            await self._verify_table_layout(cropped_image, table_info)
-        else:
-            print("TABLE INFORMATION===========================================",table_info)
-           
-            self.extracted_tables.append(table_info)
+if self.advance_table_filter:
+    await self._verify_table_layout(cropped_image, table_info)
+else:
+    [self.extracted_tables.append(info) if info['table_confidence'] > self.table_threshold_score else self.extracted_figures.append(info) for info in doclist]
+    print("Processed TABLE INFORMATION")
