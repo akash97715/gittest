664b37422b7314986fb882781c17b580e172cf9e
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-09 12:34:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
-from PIL import Image, ImageFile
-
-# Option 1: Increase the maximum image size to avoid the DecompressionBomb error
-Image.MAX_IMAGE_PIXELS = None  # This removes the limit entirely. Use with caution.
-
-# Option 2: Safely load the image, handle DecompressionBomb error
-try:
-    image = Image.open('path_to_your_large_image.jpg')
-    image.load()  # This is where the error might be triggered
-except Image.DecompressionBombError:
-    print("Image too large and risky to load, skipping...")
-    # Here, you can decide to skip this image or apply other logic
+const getToken = async () => {
+    const clientId = pm.environment.get("client_id");
+    const clientSecret = pm.environment.get("client_secret");
+    const url = pm.environment.get("pingFederateURL");
+ 
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+           
+        },
+        body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+        ignoreErrors: true
+    };
+ 
+    pm.sendRequest(url, requestOptions, function(err, response) {
+        if (err) {
+            console.error("Request failed", err);
+        } else if (!response.ok) {
+            console.error("Failed to get token", response.status, response.text());
+        } else {
+            const jsonResponse = response.json();
+            pm.environment.set("token", jsonResponse.access_token);
+            console.log("Token fetched and set successfully:", jsonResponse.access_token);
+        }
+    });
+};
+ 
+if (!pm.environment.get("token")) {
+    console.log("Fetching new token...");
+    t=getToken();
+    console.log("Fetching new token...",t);
+} else {
+    console.log("Using existing token:", pm.environment.get("token"));
+}
