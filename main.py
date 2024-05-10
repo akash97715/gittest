a6ba9f2815187584e65fe383c543c9f913d5eeff
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-10 14:53:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-    def append_metadata_to_content(self, content_list, metadata_list):
-        # Mapping metadata to UUIDs
-        metadata_map = {meta['docinsight_custom_extraction_id_key']: meta for meta in metadata_list}
-        for item in content_list:
-            uuid = item['uuid']
-            # Append metadata to the content list where UUIDs match
-            if uuid in metadata_map:
-                item['metadata'] = metadata_map[uuid]
-        return content_list
+def append_metadata_to_content(self, content_list, metadata_list):
+    # Sort metadata list by 'filename' and 'pagenumber' 
+    metadata_list.sort(key=lambda x: (x['filename'], x['pagenumber']))
+
+    # Mapping metadata to UUIDs
+    metadata_map = {meta['docinsight_custom_extraction_id_key']: meta for meta in metadata_list}
+    
+    # Append metadata to the content list where UUIDs match
+    for item in content_list:
+        uuid = item['uuid']
+        if uuid in metadata_map:
+            item['metadata'] = metadata_map[uuid]
+    
+    return content_list
