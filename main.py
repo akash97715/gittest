fa947e043fdf6642b0bfb6df65f6aea51c47e5ba
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-14 13:03:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,5 @@
-# DB_NAME="docinsightdb"
-# DB_USERNAME="docinsightuser"
-# DB_PASSWORD="aVolrvMfJbhp"
-# DB_ENDPOINT="dev-vsl-docinsight-insta
-DB_NAME="docinsightdb"
-DB_USERNAME="postgres"
-DB_PASSWORD="admin"
-DB_ENDPOINT="localhost"
-ENV="sbx"
-CLIENT_ID = "6f90ab7409494cdfb67e09de2d
-# CLIENT_SECRET = "07eB1e723dc548E99fB8
-CLIENT_SECRET = "c1c19d8e8B264cA394a7f8
-IAS_OPENAI_URL="https://mule4api-comm-a
-IAS_OPENAI_CHAT_URL="https://mule4api-c
-PINGFEDERATE_URL="https://devfederate.p
-IAS_EMBEDDINGS_URL="https://mule4api-co
-IAS_BEDROCK_URL="https://mule4api-comm-
-IS_LOCAl="true"
-UNSTRUCTURED_LOADER_ENABLED="false"
-PFIZER_OAUTH2_ENV_ID=1
-PFIZER_OAUTH2_VALIDATION_CLIENT_ID=Vess
-PFIZER_OAUTH2_VALIDATION_CLIENT_SECRET=
-OCR_BUCKET_NAME=sbx-docinsight-ocr
-DOCS_BUCKET_NAME=sbx-vsl-docinsight-doc
-TEXTRACT_ENABLED=true
-#PARENT_CHUNKS_BUCKET_NAME=sbx-poc-vsl-
-PARENT_CHUNKS_BUCKET_NAME=dev-vsl-docin
- 
- 
- 
-UNSTRUCTUREDEXCELLOADER_ENABLED=true
-REDIS_ENV=local
-REDIS_KEY_PREFIX=docinsight
-REDIS_HOST_PORT=rediss://master.vessel-
-REDIS_PASSWORD=xnxklajdjadwexnwdpeferhj
-MODEL_TOKEN_MAPPING={"gpt-35-turbo":"40
+content_list = await content_fetcher.fetch_content_from_uuids_or_type(
+            uuids,
+            uuids_request.content_type.value,
+            uuids_request.request_id
+        )
