81216ab13c7480b644699e8d81771042508555b2
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-14 13:05:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 content_list = await content_fetcher.fetch_content_from_uuids_or_type(
-            uuids,
-            uuids_request.content_type.value,
-            uuids_request.request_id
-        )
+    uuids,
+    uids_request.content_type.value if uids_request.content_type is not None else None,
+    uuids_request.request_id
+)
