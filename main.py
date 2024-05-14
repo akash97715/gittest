6472f76b4716ec3e81f3a96f4d6d4c965aca8c7f
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-14 20:28:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-content_list = await content_fetcher.fetch_content_from_uuids_or_type(
-    uuids,
-    uids_request.content_type.value if uids_request.content_type is not None else None,
-    uuids_request.request_id
-)
+table.scan(FilterExpression=Attr('r_param_1').contains('textract')
