71a5ff91ef4f1559abda697eaf5c6cf86a486a41
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-15 13:17:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 fields @timestamp, @message, @logStream, @log
 | filter @message like /localhost/ and @message like /textract/
-| filter @timestamp >= '2024-05-15T07:16:00.000Z' and @timestamp <= '2024-05-15T07:18:00.000Z'
+| filter @timestamp >= '2024-05-15T00:00:00.000+05:30' and @timestamp <= '2024-05-15T23:59:59.999+05:30'
 | sort @timestamp desc
 | limit 1000
