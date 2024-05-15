cda4a7c73d53a3c0cc4d2ea52bd0ea3d570d2e4f
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-15 12:15:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-additional_metadata_details['metering_data']['totalPages'] if not additional_metadata_details['metering_data']['advTableFilter'] else additional_metadata_details['metering_data']['totalPages']+additional_metadata_details['metering_data']['totalTable']
+total_pages = additional_metadata_details.get('metering_data', {}).get('totalPages', 0)
+adv_table_filter = additional_metadata_details.get('metering_data', {}).get('advTableFilter', False)
+total_table = additional_metadata_details.get('metering_data', {}).get('totalTable', 0)
+
+result = total_pages if not adv_table_filter else total_pages + total_table
