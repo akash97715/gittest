cae4d612ae7061dba0c936a8b5f00a0c673e0e97
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-15 13:10:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-2024-05-15T12:36:00.967+05:30
+fields @timestamp, @message, @logStream, @log
+| parse @timestamp "*T*Z" as date, time
+| filter date = '2024-05-15' and time >= '12:46:00' and time <= '12:48:00'
+| filter @message like /localhost/ and @message like /textract/
+| sort @timestamp desc
+| limit 1000
+
+
+
+
+fields @timestamp, @message, @logStream, @log
+| filter @message like /localhost/ and @message like /textract/
+| filter @timestamp >= '2024-05-15T12:46:00.000+05:30' and @timestamp <= '2024-05-15T12:48:00.000+05:30'
+| sort @timestamp desc
+| limit 1000
