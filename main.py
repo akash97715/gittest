4973748ec979404d18c769604007be3aefc087f1
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-23 14:19:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,44 @@
-from typing import List
-
-class IASOpenaiEmbeddings(Embeddings):
-    """Wrapper for IAS secured OpenAI embedding API"""
-    
-    engine = str
-    client_id: str = None
-    x_vsl_client_id: str = None
-    bearer_auth: str = None
-    totalToken: list
-    
-    def __init__(self, engine, client_id, totalToken, x_vsl_client_id=None, bearer_auth=None):
-        self.engine = engine
-        self.totalToken = totalToken
-        self.client_id = client_id
-        self.x_vsl_client_id = x_vsl_client_id
-        self.bearer_auth = bearer_auth
-    
-    def embed_documents(self, texts: List[str]) -> List[List[float]]:
-        """Embeds search docs."""
-        try:
-            response, totaltok = ias_openai_embeddings(
-                texts, self.engine, self.client_id, self.x_vsl_client_id, self.bearer_auth
+def ias_openai_embeddings(
+    raw_text:list,
+    engine: str,
+    client_id: str = None,
+    x_vsl_client_id: str = None,
+    bearer_token: str = None,
+):
+    try:
+        url = IAS_EMBEDDINGS_URL
+        payload = {"input": raw_text, "engine": engine}
+        token = get_auth_token(bearer_token)
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": f"Bearer {token}",
+        }
+ 
+        if x_vsl_client_id is not None:
+            headers["x-vsl-client_id"] = x_vsl_client_id
+        elif client_id is not None:
+            headers["x-vsl-client_id"] = client_id
+ 
+        logger.info("Triggering embedding endpoint")
+        response = requests.post(url, headers=headers, json=payload)
+ 
+        if response.status_code != 200:
+            logger.error(
+                f"Error calling OpenAI embedding API: {response.status_code}, {response.json()}"
             )
-            self.totalToken.append(totaltok)
-            
-            # Extract the embeddings
-            embeddings: list[list[float]] = [data["embedding"] for data in response]
-            return embeddings
-        except Exception as e:
-            self.handle_error(e)
-            raise
-    
-    def embed_query(self, text: str) -> List[float]:
-        """Embeds query text."""
-        try:
-            response, totaltok = ias_openai_embeddings(
-                text, self.engine, self.client_id, self.x_vsl_client_id, self.bearer_auth
+            raise GenericException(
+                f"Error calling OpenAI embedding API: {response.status_code}, {response.json()}",
+                status_code=response.status_code,
             )
-            self.totalToken.append(totaltok)
-            
-            # Extract the embeddings
-            embeddings: list[list[float]] = [data["embedding"] for data in response]
-            return embeddings[0]
-        except Exception as e:
-            self.handle_error(e)
-            raise
-    
-    def handle_error(self, error: Exception):
-        """Handles errors by calling push_metric with the sum of totalToken."""
-        total_tokens_sum = sum(self.totalToken)
-        self.push_metric(total_tokens_sum)
-    
-    def push_metric(self, total_tokens: int):
-        """Placeholder function to push metrics."""
-        print(f"Pushing metric: Total Tokens = {total_tokens}")
+ 
+        embeddings = json.loads(response.json()["result"])
+        temp=response.json()
+        total_token=temp['totalTokens']        
+        logger.info("Recevied response from embedding endpoint")
+ 
+        return embeddings,total_token
+    except Exception as e:
+        logger.error("Got the Exception", str(e))
+        # raising backoff exception
+        raise GenericException(e
