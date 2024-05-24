88e5d0c8ab871514b5b004e2f235df27686651a7
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-24 12:35:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-[12:32 PM] Deep, Akash (External)
-        metering_data={'totalPages':total_pages,'totalTable':self.total_extracted_tables, 'filteredTable':len(self.extracted_tables),  'advTableFilter':self.advance_table_filter}
-       
-        total_pages_temp = metering_data.get('metering_data', {}).get('totalPages', 0)
-        adv_table_filter = metering_data.get('metering_data', {}).get('advTableFilter', False)
-        total_table = metering_data.get('metering_data', {}).get('totalTable', 0)
-        total_pages = total_pages_temp if not adv_table_filter else total_pages_temp + total_table
+# Initialize metering_data dictionary
+metering_data = {
+    'totalPages': total_pages,
+    'totalTable': self.total_extracted_tables,
+    'filteredTable': len(self.extracted_tables),
+    'advTableFilter': self.advance_table_filter
+}
+
+# Retrieve values from metering_data
+total_pages_temp = metering_data.get('totalPages', 0)
+adv_table_filter = metering_data.get('advTableFilter', False)
+total_table = metering_data.get('totalTable', 0)
+
+# Determine the final value of total_pages based on adv_table_filter
+total_pages = total_pages_temp if not adv_table_filter else total_pages_temp + total_table
