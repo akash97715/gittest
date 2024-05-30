4d2baf553d95d823f8b441b3e2f85c6e3c6197c9
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-30 11:40:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-"openai" if param['engine'].startswith("gpt") else "bedrock")
+metering_dict=[{'engine':rw.embedding_engine,'total_token':total_token[1]},{'engine':rw.llm_engine,'total_token':total_token[0]}]
