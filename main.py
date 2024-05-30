9b5d8fd8ed373c549ac4213255d6166ed2fd5fd9
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-30 10:26:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-push_metrics(req_arrival_time=request.headers["x_agw_request_time"],
-                                    client_id=request.headers["x-agw-client_id"],
-                                    api_id=request.headers["x_agw_api_id"],
-                                    url=str(request.url),
-                                    uom_val=total_pages,
-                                    engine=rw.engine,
-                                    purpose="openai")
+"openai" if param['engine'].startswith("gpt") else "bedrock")
