40731e0c5ea9c549a0108b62a89d23cb66df1756
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-30 16:58:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,34 @@
 Statement:
-              - Effect: Allow
-                Action:
-                  - logs:CreateLogGroup
-                  - logs:CreateLogStream
-                  - logs:PutLogEvents
-                Resource: arn:aws:logs:*:*:*
-              - Effect: Allow
-                Action:
-                  - logs:CreateLogGroup
-                  - logs:CreateLogStream
-                  - logs:PutLogEvents
-                Resource: !Sub "arn:aws:logs:*:${AWS::AccountId}:log-group:/aws/lambda/${Env}-vsl-${Name}-ingestion-embedding*"
-              - Effect: Allow
-                Action:
-                  - logs:CreateLogGroup
-                  - logs:CreateLogStream
-                  - logs:PutLogEvents
-                Resource: !Sub "arn:aws:logs:*:${AWS::AccountId}:log-group:/aws/lambda/${Env}-vsl-${Name}-ingestion-store*"
-              - Effect: Allow
-                Action:
-                  - logs:CreateLogGroup
-                  - logs:CreateLogStream
-                  - logs:PutLogEvents
-                Resource: !Sub "arn:aws:logs:*:${AWS::AccountId}:log-group:/aws/states/${Env}-vsl-${Name}-ingestion*"
-              - Effect: Allow
-                Action:
-                  - ec2:DescribeNetworkInterfaces
-                  - ec2:CreateNetworkInterface
-                  - ec2:DeleteNetworkInterface
-                  - ec2:DescribeInstances
-                  - ec2:AttachNetworkInterface
-                Resource: "*"
-              - Effect: Allow
-                Action:
-                  - "secretsmanager:GetSecretValue"
-                Resource: !Sub "arn:aws:secretsmanager:*:${AWS::AccountId}:secret:${Env}-vsl-${Name}-secrets*"
-              - Effect: Allow
-                Action:
-                  - "lambda:InvokeFunction"
-                Resource: !Sub "arn:aws:lambda:*:${AWS::AccountId}:function:${Env}-vsl-${Name}-ingestion-process"
-              - Effect: Allow
-                Action:
-                  - "lambda:InvokeFunction"
-                Resource: !Sub "arn:aws:lambda:*:${AWS::AccountId}:function:${Env}-vsl-${Name}-ingestion-embedding"
-              - Effect: Allow
-                Action:
-                  - "lambda:InvokeFunction"
-                Resource: !Sub "arn:aws:lambda:*:${AWS::AccountId}:function:${Env}-vsl-${Name}-ingestion-store"
-              - Effect: Allow
-                Action:
-                  - sqs:*
-                  - sqs:GetQueueUrl
-                Resource: !Sub "arn:aws:sqs:*:${AWS::AccountId}:${Env}-vsl-${Name}-*"
+  - Effect: Allow
+    Action:
+      - logs:CreateLogGroup
+      - logs:CreateLogStream
+      - logs:PutLogEvents
+    Resource:
+      - !Sub "arn:aws:logs:*:${AWS::AccountId}:log-group:/aws/lambda/${Env}-vsl-${Name}-ingestion-embedding*"
+      - !Sub "arn:aws:logs:*:${AWS::AccountId}:log-group:/aws/lambda/${Env}-vsl-${Name}-ingestion-store*"
+      - !Sub "arn:aws:logs:*:${AWS::AccountId}:log-group:/aws/states/${Env}-vsl-${Name}-ingestion*"
+  - Effect: Allow
+    Action:
+      - ec2:DescribeNetworkInterfaces
+      - ec2:CreateNetworkInterface
+      - ec2:DeleteNetworkInterface
+      - ec2:DescribeInstances
+      - ec2:AttachNetworkInterface
+    Resource: "*"
+  - Effect: Allow
+    Action:
+      - "secretsmanager:GetSecretValue"
+    Resource: !Sub "arn:aws:secretsmanager:*:${AWS::AccountId}:secret:${Env}-vsl-${Name}-secrets*"
+  - Effect: Allow
+    Action:
+      - "lambda:InvokeFunction"
+    Resource:
+      - !Sub "arn:aws:lambda:*:${AWS::AccountId}:function:${Env}-vsl-${Name}-ingestion-process"
+      - !Sub "arn:aws:lambda:*:${AWS::AccountId}:function:${Env}-vsl-${Name}-ingestion-embedding"
+      - !Sub "arn:aws:lambda:*:${AWS::AccountId}:function:${Env}-vsl-${Name}-ingestion-store"
+  - Effect: Allow
+    Action:
+      - sqs:*
+      - sqs:GetQueueUrl
+    Resource: !Sub "arn:aws:sqs:*:${AWS::AccountId}:${Env}-vsl-${Name}-*"
