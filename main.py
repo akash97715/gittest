4ed89d261388552383b31ade36fc5ca4e2f9536f
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-30 11:42:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-metering_dict=[{'engine':rw.embedding_engine,'total_token':total_token[1]},{'engine':rw.llm_engine,'total_token':total_token[0]}]
+metering_dict = [{'engine': rw.embedding_engine, 'total_token': total_token[1]}] + ([{'engine': rw.llm_engine, 'total_token': total_token[0]}] if llm_response_flag else [])
