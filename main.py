f523d5c02534979294ebf17f2747bde155191ac9
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-30 16:48:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-metering_dict = [{'engine': rw.embedding_engine, 'total_token': total_token[1]}] + ([{'engine': rw.llm_engine, 'total_token': total_token[0]}] if llm_response_flag else [])
+The libzip package has below limitations identified:
+Formdata endpoints are not supported – need for support
+Any request that does not have a JSON payload fails to be metered (this includes not just GET/DELETE but also POST or PUT endpoints that have an optional payload) – need for support of endpoints that are metered even without a JSON payload
