e2728c9a591d2e4fe1d80d65fee3ff2571b72455
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-05-30 10:09:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,7 @@
-{
-  "Comment": "State Machine to ensure all embeddings are created for parquet file",
-  "StartAt": "ProcessParquetFile",
-  "States": {
-    "ProcessParquetFile": {
-      "Type": "Task",
-      "Resource": "arn:aws:lambda:REGION:ACCOUNT_ID:function:ProcessParquetLambda",
-      "ResultPath": "$.processResult",
-      "Next": "CheckFileType"
-    },
-    "CheckFileType": {
-      "Type": "Choice",
-      "Choices": [
-        {
-          "Variable": "$.processResult.fileType",
-          "StringEquals": "parquet",
-          "Next": "LoadParquetAndProcess"
-        },
-        {
-          "Or": [
-            {
-              "Variable": "$.processResult.fileType",
-              "StringEquals": "csv"
-            },
-            {
-              "Variable": "$.processResult.fileType",
-              "StringEquals": "xlsx"
-            }
-          ],
-          "Next": "Success"
-        }
-      ],
-      "Default": "Fail"
-    },
-    "LoadParquetAndProcess": {
-      "Type": "Task",
-      "Resource": "arn:aws:lambda:REGION:ACCOUNT_ID:function:ProcessLambda",
-      "ResultPath": "$.lambdaResult",
-      "Next": "CheckCompletion"
-    },
-    "CheckCompletion": {
-      "Type": "Choice",
-      "Choices": [
-        {
-          "Variable": "$.lambdaResult.complete",
-          "BooleanEquals": true,
-          "Next": "Success"
-        },
-        {
-          "Variable": "$.lambdaResult.complete",
-          "BooleanEquals": false,
-          "Next": "LoadParquetAndProcess"
-        }
-      ],
-      "Default": "Fail"
-    },
-    "Success": {
-      "Type": "Succeed"
-    },
-    "Fail": {
-      "Type": "Fail",
-      "Error": "ProcessFailed",
-      "Cause": "The embeddings creation process failed."
-    }
-  }
-}
+push_metrics(req_arrival_time=request.headers["x_agw_request_time"],
+                                    client_id=request.headers["x-agw-client_id"],
+                                    api_id=request.headers["x_agw_api_id"],
+                                    url=str(request.url),
+                                    uom_val=total_pages,
+                                    engine=rw.engine,
+                                    purpose="openai")
