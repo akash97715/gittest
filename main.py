5645469a3408fa0c4b23137572aac322a6ce0ff5
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-03 16:13:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 POST kibana_sample_data_ecommerce/_search
 {
   "query": {
-    "term": {
-      "metadata.md5": "7268768"
+    "more_like_this": {
+      "fields": ["docs"],
+      "like": "what is there in docs",
+      "min_term_freq": 1,
+      "min_doc_freq": 1
     }
   }
 }
