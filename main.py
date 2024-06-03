93ecf433694aece07baadbc67be456e60a0eb469
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-03 16:24:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-kl=vector_db.similarity_search(test_query,kwargs={"filter":{'terms': { "metadata.md5":["3863033b4e4ff29fe6125b2d2efcae9e"]}},"k":400})
+kl = vector_db.similarity_search(
+    test_query,
+    kwargs={
+        "filter": {
+            "bool": {
+                "must": [
+                    {
+                        "terms": {
+                            "metadata.md5": ["3863033b4e4ff29fe6125b2d2efcae9e"]
+                        }
+                    }
+                ]
+            }
+        },
+        "k": 400
+    }
+)
