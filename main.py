5e4376c29317bd4bac107c453164207c1e30a218
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-04 16:44:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 PUT /cosinesimilarity/_mapping
-{"properties": {"vector_field": {
-          "type": "knn_vector",
-          "dimension": 1536,
-          "method": {
-            "engine": "nmslib",
-            "space_type": "l2",
-            "name": "hnsw",
-            "parameters": {
-              "ef_construction": 512,
-              "m": 16
-            }
-          }
-        }}}
+{
+  "properties": {
+    "vector_field": {
+      "type": "knn_vector",
+      "dimension": 1536,
+      "method": {
+        "name": "hnsw",
+        "space_type": "cosinesimil",
+        "engine": "nmslib",
+        "parameters": {
+          "ef_construction": 512,
+          "m": 16
+        }
+      }
+    }
+  }
+}
