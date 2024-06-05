d4a97d17cc1f5210ab2edc01465c15207fcfe626
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-05 11:29:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-"vector_field": {
-          "type": "knn_vector",
-          "dimension": 1536,
-          "method": {
-            "engine": "nmslib",
-            "space_type": "cosinesimil",
-            "name": "hnsw",
-            "parameters": {
-              "ef_construction": 512,
-              "m": 16
-            }
+PUT /your_index_name
+{
+  "mappings": {
+    "properties": {
+      "vector_field": {
+        "type": "knn_vector",
+        "dimension": 1536,
+        "method": {
+          "engine": "nmslib",
+          "space_type": "cosinesimil",
+          "name": "hnsw",
+          "parameters": {
+            "ef_construction": 512,
+            "m": 16
           }
         }
+      }
+    }
+  }
+}
