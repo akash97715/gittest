228570a0087d0dada318bf74f66d5d01c2fddab6
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-05 12:41:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-POST /_snapshot/biopharma-cosine-repository/biopharmacosine/_restore?wait_for_completion=true
 {
-  "indices": "9cf1f92f762b222a1a72792a4c4ba9e9",  // original index name in the snapshot
-  "include_global_state": false,
-  "rename_pattern": "9cf1f92f762b222a1a72792a4c4ba9e9",  // original index name in the snapshot
-  "rename_replacement": "test-mapping-updated"  // new index name
+  "error": {
+    "root_cause": [
+      {
+        "type": "snapshot_restore_exception",
+        "reason": "[biopharma-cosine-repository:biopharmacosine/HO85El7TRoilNrMCpApN2g] cannot restore index [test-mapping-updated] because an open index with same name already exists in the cluster. Either close or delete the existing index or restore the index under a different name by providing a rename pattern and replacement name"
+      }
+    ],
+    "type": "snapshot_restore_exception",
+    "reason": "[biopharma-cosine-repository:biopharmacosine/HO85El7TRoilNrMCpApN2g] cannot restore index [test-mapping-updated] because an open index with same name already exists in the cluster. Either close or delete the existing index or restore the index under a different name by providing a rename pattern and replacement name"
+  },
+  "status": 500
 }
