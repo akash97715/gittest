9493138c3d06409a1a1da702bb04ae56f61e5d88
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-05 12:39:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-PUT /_snapshot/biopharma-cosine-repository/biopharmacosine?wait_for_completion=true
+POST /_snapshot/biopharma-cosine-repository/biopharmacosine/_restore?wait_for_completion=true
 {
-"indices": "9cf1f92f762b222a1a72792a4c4ba9e9",
-"ignore_unavailable": true,
-"include_global_state": false,
-"partial": false
-}
- 
- 
-POST _snapshot/biopharma-cosine-repository/biopharmacosine/_restore?wait_for_completion=true
-{
-"indices": "test-mapping-updated",
-"include_global_state": false
+  "indices": "9cf1f92f762b222a1a72792a4c4ba9e9",  // original index name in the snapshot
+  "include_global_state": false,
+  "rename_pattern": "9cf1f92f762b222a1a72792a4c4ba9e9",  // original index name in the snapshot
+  "rename_replacement": "test-mapping-updated"  // new index name
 }
