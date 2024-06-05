816a3e5739063e4d0e6704183cd5022ee66e3ad9
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-05 12:28:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
+POST _snapshot/your-repository-name/dev-snapshot/_restore?wait_for_completion=true
 {
-  "error": {
-    "root_cause": [
-      {
-        "type": "illegal_argument_exception",
-        "reason": "unknown setting [index.provided_name] please check that any required plugins are installed, or check the breaking changes documentation for removed settings"
-      }
-    ],
-    "type": "illegal_argument_exception",
-    "reason": "unknown setting [index.provided_name] please check that any required plugins are installed, or check the breaking changes documentation for removed settings"
-  },
-  "status": 400
+"indices": "Indexname-1, index-name2",
+"include_global_state": false
 }
