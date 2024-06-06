a10eef1897a1208ffc871073ab63b58f37b21f85
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-06 18:09:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import uuid
-
-client_id = 'LDPTRU'  # Example client_id
-index_name = 'your_index_name'
-
-id_key = "{}/{}/{}".format(
-    client_id if not (len(client_id) == 36 and all(c in '0123456789abcdef-' for c in client_id.lower())) else str(uuid.UUID(client_id)), 
-    index_name, 
-    uuid.uuid4()
-)
-
-print(id_key)
+search_type: Optional[str]='approximate_search'
+ 
+ docs = vector_db.similarity_search_with_relevance_scores(
+                query=rw.user_query,
+                k=rw.num_of_citations,
+                filter=search_kwargs,
+                score_threshold=score_threshold,
+            )
