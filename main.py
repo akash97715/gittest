5d47bf94cc5da8eba938cc7347fe58fbe33666ed
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-06 18:18:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-search_type: Optional[str]='approximate_search'
- 
- docs = vector_db.similarity_search_with_relevance_scores(
-                query=rw.user_query,
-                k=rw.num_of_citations,
-                filter=search_kwargs,
-                score_threshold=score_threshold,
-            )
+docs = vector_db.similarity_search_with_relevance_scores(
+    query=rw.user_query,
+    k=rw.num_of_citations,
+    score_threshold=score_threshold,
+    **({'pre_filter': search_kwargs, 'search_type': 'painless_scripting', 'space_type': 'cosine'} if search_type == 'painless_scripting' and space_type == 'cosine' else {'filter': search_kwargs})
+)
