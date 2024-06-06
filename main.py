9c218e6eb374fe7082d8e7b3071acaa1b8877ec1
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-06 19:17:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+search_kwargs_dict = {'filter': search_kwargs}
+
+if rw.search_type == 'painless_scripting':
+    search_kwargs_dict = {'pre_filter': search_kwargs, 'search_type': 'painless_scripting'}
+    if rw.space_type:
+        search_kwargs_dict['space_type'] = rw.space_type
+
 search_params = {
     'query': rw.user_query,
     'k': rw.num_of_citations,
     'score_threshold': score_threshold,
-    **({'pre_filter': search_kwargs, 'search_type': 'painless_scripting', 'space_type': rw.space_type} if rw.search_type == 'painless_scripting' else {'filter': search_kwargs})
+    **search_kwargs_dict
 }
-
-if rw.space_type:
-    search_params['space_type'] = rw.space_type
 
 # Print or log the search parameters
 print("Search parameters:", search_params)
