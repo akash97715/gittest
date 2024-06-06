93bd6a690589bca6caebf152fbbe0e29b01f6a91
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-06 18:49:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-docs = vector_db.similarity_search_with_relevance_scores(
-    query=rw.user_query,
-    k=rw.num_of_citations,
-    score_threshold=score_threshold,
-    space_type=space_type,
-    **({'pre_filter': search_kwargs, 'search_type': 'painless_scripting'} if search_type == 'painless_scripting' else {'filter': search_kwargs})
-)
+[6:48 PM] Deep, Akash (External)
+docs = vector_db.similarity_search_with_score(
+                        query=rw.user_query,
+                        k=rw.num_of_citations,
+                        score_threshold=score_threshold,
+                        space_type=rw.space_type,
+                        **({'pre_filter': search_kwargs, 'search_type': 'painless_scripting'} if rw.search_type == 'painless_scripting' else {'filter': search_kwargs})
+                    )
