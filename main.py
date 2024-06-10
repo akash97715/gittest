e9d66616b9a75daa368890d6f1fb398712306bf5
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-10 20:03:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,208 +1,67 @@
-class IAS_ChatModel(BaseChatModel,BaseModel):
-    engine: str
-    temperature: float
-    max_tokens: int
-    user_query: str
-    total_consumed_token: List[int] = Field(default_factory=list)
-    min_response_token: int
-    system_message: Optional[str] = (None,)
-    client_id: str = (None,)
-    x_vsl_client_id: str = None
-    bearer_token: str = None
-    context: list = None
-
-    class Config:
-        arbitrary_types_allowed = True    
-
-    async def _agenerate(
-        self,
-        messages: List[BaseMessage],
-        stop: Optional[List[str]] = None,
-        run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
-        **kwargs: Any,
-    ) -> ChatResult:
-        """Top Level call"""
-        messages_dict = [convert_message_to_dict(s) for s in messages]
-
-        # Create chat history array.
-        if self.context:
-            chat_history = create_chat_history_array(self.context)
-            messages_dict[1:1] = chat_history
-
-        if not is_anthropic_model(str(self.engine)):
-            # Reduce max token based on token consumed
-            all_msgs = ""
-            for message in messages_dict:
-                all_msgs += str(message["content"])
-
-            token_consumed = (
-                self.get_num_tokens(
-                    (
-                        "" + all_msgs + self.user_query + json.dumps(kwargs["tools"])
-                        if kwargs["tools"]
-                        else ""
-                    )
-                )
-                + self.min_response_token
-            )
-            if self.max_tokens - token_consumed <= 0:
-                token_consumed = 0
-
-            logger.info(
-                f"total token by system_message, user_query, kwargs[tools] is - {token_consumed}"
-            )
-        else:
-            token_consumed = 0
-
-        response ,total_token_completion= await ias_openai_chat_completion_with_tools(
-            self.engine,
-            self.temperature,
-            self.max_tokens - token_consumed,
-            self.client_id,
-            self.x_vsl_client_id,
-            self.bearer_token,
-            messages_dict,
-            kwargs["tools"],
-            "auto",
-        )
-        logger.debug(f"Total tokens consumed: {total_token_completion}")
-        
-        self.total_consumed_token.append(total_token_completion)
-
-        
-
-        return self._create_chat_result(response)
-
-    def _generate(
-        self,
-        messages: List[BaseMessage],
-        stop: Optional[List[str]] = None,
-        run_manager: Optional[CallbackManagerForLLMRun] = None,
-        **kwargs: Any,
-    ) -> ChatResult:
-        pass
-
-    def _create_chat_result(
-        self, response: Union[dict, openai.BaseModel, str]
-    ) -> ChatResult:
-        generations = []
-
-        gen = ChatGeneration(
-            message=convert_dict_to_message(response),
-            generation_info=dict(finish_reason="stop"),
-        )
-        generations.append(gen)
-        llm_output = {
-            "token_usage": 0,
-            "model_name": self.engine,
-        }
-        return ChatResult(generations=generations, llm_output=llm_output)
-
-    @property
-    def _llm_type(self) -> str:
-        """Get the type of language model used by this chat model."""
-        return "IAS_OpenAI"
-
-
-def create_chat_history_array(context: list) -> list[dict]:
-    chat_context = copy.deepcopy(context)
-    chat_context.reverse()
-    chat_history = []
-
-    for i in range(len(context)):
-        if i % 2 == 0:
-            chat_history.append({"role": "user", "content": chat_context[i]})
-        else:
-            chat_history.append({"role": "assistant", "content": chat_context[i]})
-
-    return chat_history
-
-
-def convert_dict_to_message(_dict: Mapping[str, Any]) -> BaseMessage:
-    """Convert a dictionary to a LangChain message.
-
-    Args:
-        _dict: The dictionary.
-
-    Returns:
-        The LangChain message.
+async def ias_openai_chat_completion_with_tools(
+    engine: str,
+    temperature: float,
+    max_tokens: int,
+    client_id: str = None,
+    x_vsl_client_id: str = None,
+    bearer_token: str = None,
+    messages: List[BaseMessage] = None,
+    tools: List[BaseTool] = None,
+    tool_choice: str = None,
+) -> str:
     """
-    role = _dict.get("role")
-    id_ = _dict.get("id")
-    if role == "user":
-        return HumanMessage(content=_dict.get("content", ""), id=id_)
-    elif role == "assistant":
-        # Fix for azure
-        # Also OpenAI returns None for tool invocations
-        content = _dict.get("content", "") or ""
-        additional_kwargs: Dict = {}
-        if function_call := _dict.get("function_call"):
-            additional_kwargs["function_call"] = dict(function_call)
-        if tool_calls := _dict.get("tool_calls"):
-            additional_kwargs["tool_calls"] = tool_calls
-        return AIMessage(content=content, additional_kwargs=additional_kwargs, id=id_)
-    elif role == "system":
-        return SystemMessage(content=_dict.get("content", ""), id=id_)
-    elif role == "function":
-        return FunctionMessage(
-            content=_dict.get("content", ""), name=_dict.get("name"), id=id_
-        )
-    elif role == "tool":
-        additional_kwargs = {}
-        if "name" in _dict:
-            additional_kwargs["name"] = _dict["name"]
-        return ToolMessage(
-            content=_dict.get("content", ""),
-            tool_call_id=_dict.get("tool_call_id"),
-            additional_kwargs=additional_kwargs,
-            id=id_,
-        )
-    else:
-        return ChatMessage(content=_dict.get("content", ""), role=role, id=id_)
-
-
-def convert_message_to_dict(message: BaseMessage) -> dict:
-    """Convert a LangChain message to a dictionary.
-
-    Args:
-        message: The LangChain message.
-
-    Returns:
-        The dictionary.
+    Generates a chat completion response for OpenAI model
+    :param token: auth token
+    :param user_message: user's prompt
+    :param engine: model capable for chat completion i.e. gpt*
+    :param temperature: value 0-1 that tells model to be more precise or generative
+    :param max_tokens: max tokens the prompt & response should be. It depends on the model's capacity
+    :return: response from OpenAI model
     """
-    message_dict: Dict[str, Any]
-    if isinstance(message, ChatMessage):
-        message_dict = {"role": message.role, "content": message.content}
-    elif isinstance(message, HumanMessage):
-        message_dict = {"role": "user", "content": message.content}
-    elif isinstance(message, AIMessage):
-        message_dict = {"role": "assistant", "content": message.content}
-        if "function_call" in message.additional_kwargs:
-            message_dict["function_call"] = message.additional_kwargs["function_call"]
-            # If function call only, content is None not empty string
-            if message_dict["content"] == None:
-                message_dict["content"] = ""
-        if "tool_calls" in message.additional_kwargs:
-            message_dict["tool_calls"] = message.additional_kwargs["tool_calls"]
-            # If tool calls only, content is None not empty string
-            if message_dict["content"] == None:
-                message_dict["content"] = ""
-    elif isinstance(message, SystemMessage):
-        message_dict = {"role": "system", "content": message.content}
-    elif isinstance(message, FunctionMessage):
-        message_dict = {
-            "role": "function",
-            "content": message.content,
-            "name": message.name,
+    try:
+        payload = {
+            "engine": engine,
+            "messages": messages,
+            "temperature": temperature,
+            "max_tokens": max_tokens,
+            "tools": tools,
+            "tool_choice": tool_choice,
         }
-    elif isinstance(message, ToolMessage):
-        message_dict = {
-            "role": "tool",
-            "content": message.content,
-            "tool_call_id": message.tool_call_id,
+ 
+        token = await aget_auth_token(bearer_token)
+ 
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {token}",
         }
-    else:
-        raise TypeError(f"Got unknown type {message}")
-    if "name" in message.additional_kwargs:
-        message_dict["name"] = message.additional_kwargs["name"]
-    return message_dict
+        if x_vsl_client_id is not None:
+            headers["x-vsl-client_id"] = x_vsl_client_id
+        elif client_id is not None:
+            headers["x-vsl-client_id"] = client_id
+ 
+        logger.info("Calling chat completion endpoint with tools")
+        logger.info(payload)
+ 
+        response = await httpx_client.request(
+            "post", IAS_OPENAI_CHAT_URL, json=payload, headers=headers
+        )
+ 
+        logger.info("Received response from llm")
+        logger.info(response.json())
+ 
+        if response.status_code != 200:
+            logger.error(
+                f"Error calling OpenAI chat completion API: {response.status_code}, {response.json()}"
+            )
+            raise GenericException(
+                f"Error calling OpenAI chat completion API: {response.status_code}, {response.json()}",
+                status_code=response.status_code,
+            )
+        chat_completion = json.loads(response.json()["result"])
+       
+        total_token_completion=int(response.json()['totalTokens'])
+        return chat_completion,total_token_completion
+    except Exception as e:
+        logger.error("Got the Exception", str(e))
+        # raising backoff exception
+        raise GenericException(e)
