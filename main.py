654bc353ae794a5c7f808f9c70e10d5be56cf643
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-12 23:28:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,12 @@
-Tool
- 
-"agent":"retriever_agent",
- 
- 
-{
-    "id":1,
-    "name":"retriever_tool",
-    "desc": "Retrieves data from the database",
-    "type":"tool",
-    "lambda_arn":"arn:aws:lambda:us-east-1:123456789012:function:retriever_tool",
-    "req_payload": {},
-    "res_payload": {}
-}
- 
-Agents
- 
-{
-    "id":1,
-    "name":"retriever_agent",
-    "desc": "Retrieves data from the database",
-    "type":"agent",
-    "prompt":"What data would you like to retrieve?",
-    "sys_prompt": "What data would you like to retrieve?",
-    "tools":[1],
-    "llm":"",
-    "supported_llm":"",
-    // "nodes": {},
-    "edges": [{
-        "start_node": "retriever_tool",
-        "end_node": "grade_tool",
-    },
-    {
-        "start_node": "grade_tool",
-        "conditions": [{
-            "condition": "NO",
-            "start_node": "retriever_tool"
-        },{
-            "condition": "Yes",
-            "start_node": "retriever_tool"
-        }],
-        "end_node": "LLM",
-    },
-    {
-        "start_node": "LLM",
-        "conditions": {
-            "condition": "NO",
-            "start_node": "retriever_tool"
-        },
-        "end_node": "END",
-    }],
-    "entry_point": "retriever_tool",
-}
+# Add to vectorDB
+vector_db = IAS_OpenSearchVectorSearch(
+                                    index_name="32dea38ed04635d9354625041c98a6e0",
+                                    embedding_function=embd,
+                                    opensearch_url=AWS_OPENSEARCH_HOST,
+                                    http_auth=(
+                                        AWS_OPENSEARCH_USERNAME,
+                                        AWS_OPENSEARCH_PASSWORD,
+                                    ),
+                                    is_aoss=False,
+                                )
+retriever = vector_db.as_retriever()
