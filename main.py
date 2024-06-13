a5becf121acb70022bf3d14adb027bcdb4aaa9aa
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-13 12:16:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-git clone https://github.com/username/repo_name.git
-cd repo_name
-pip install -e .
+{
+    "security.workspace.trust.untrustedFiles": "open",
+    "editor.minimap.enabled": false,
+    "[python]": {
+        "editor.formatOnType": true,
+        "editor.defaultFormatter": "ms-python.black-formatter",
+        "editor.formatOnSave": true
+    },
+    "workbench.editor.enablePreview": false,
+    "terminal.integrated.scrollback": 10000,
+    "aws.samcli.location": "C:\\Program Files\\Amazon\\AWSSAMCLI\\bin\\sam.cmd"
+}
