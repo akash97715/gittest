19e1a5ae190660cb3766b14ba0645e0e30d8292f
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-18 12:07:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-df['embedding'] = df['embedding'].apply(lambda x: x[0] if isinstance(x, list) and len(x) > 0 and isinstance(x[0], list) else x)
+df['embedding'] = df['embedding'].apply(lambda x: x[0] if isinstance(x, list) and len(x) == 1 else x)
