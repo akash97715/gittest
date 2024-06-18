5c18a3461ab524b6f7f7916807b660c55f3b80b1
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-18 09:28:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-([{'object': 'embedding', 'index': 0, 'embedding': [-0.0006847794, -0.010218057, 0.052048225, -0.042628456,
+embedding = response[0]['embedding'] if response and 'embedding' in response[0] else None
