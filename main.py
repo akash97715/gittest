7de7ac24b1adc1e1e79edc9996cec02326d8282e
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-18 17:41:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,7 @@
 {
-    "user_query": "subject: fwd: pp-e1a-usa-0200 - elra us app in-service deck.pdf
-from: shss-senior health and science specialist - pfizer
-date: wed, 27 sep 2023 12:11:55 -0400
-to: wai tak sung <wsung@mhs.net>
-mike nunez
-pfizer hematology institutional specialist
-florida
-c: 786.457.4168
-shss-senior health and science specialist - pfizer
-signatureimage
-begin forwarded message:
-from: hiriart, rita maria <shss-senior health and science specialist - pfizer>
-date: september 26, 2023 at 12:29:37 pm edt
-to: nunez, michael f <shss-senior health and science specialist - pfizer>
-subject: pp-e1a-usa-0200 - elra us app in-service deck.pdf
-edit, sign and share pdf files on the go. download acrobat reader for mobile:
-https://adobeacrobat.app.link/getapp
-fyi,
-rita hiriart
-pfizer hematology senior account specialist
-miami, fl & puerto rico
-cell: (786)287-7058
-sent from my ipad",
+    "user_query": "subject: fwd: pp-e1a-usa-0200 - elra us app in-service deck.pdf\nfrom: shss-senior health and science specialist - pfizer\ndate: wed, 27 sep 2023 12:11:55 -0400\nto: wai tak sung <wsung@mhs.net>\nmike nunez\npfizer hematology institutional specialist\nflorida\nc: 786.457.4168\nshss-senior health and science specialist - pfizer\nsignatureimage\nbegin forwarded message:\nfrom: hiriart, rita maria <shss-senior health and science specialist - pfizer>\ndate: september 26, 2023 at 12:29:37 pm edt\nto: nunez, michael f <shss-senior health and science specialist - pfizer>\nsubject: pp-e1a-usa-0200 - elra us app in-service deck.pdf\nedit, sign and share pdf files on the go. download acrobat reader for mobile:\nhttps://adobeacrobat.app.link/getapp\nfyi,\nrita hiriart\npfizer hematology senior account specialist\nmiami, fl & puerto rico\ncell: (786)287-7058\nsent from my ipad",
     "metadata": {
-        "metadata": {
-            "category": "risk"
-        }
+        "category": "risk"
     },
     "context": [],
     "temperature": 0,
