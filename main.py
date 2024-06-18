ccc0c163f14e4f2bb64048173d9c7288000d212b
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-18 17:39:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
-def process_embeddings(add_embeddings_func, text_embeddings, metadatas, max_retries=3):
-    retries = 0
-    while retries < max_retries:
-        try:
-            add_embeddings_func(text_embeddings=text_embeddings, metadatas=metadatas)
-            logger.info("Successfully ingested all embeddings")
-            break
-        except BulkIndexError as e:
-            retries += 1
-            logger.error(f"BulkIndexError: {e.errors}")
-            if retries >= max_retries:
-                logger.error(f"Failed to index after {max_retries} retries")
-            else:
-                logger.info(f"Retrying, attempt {retries + 1}")
-        except Exception as e:
-            logger.exception(f"An unexpected error occurred: {e}")
-            break
- 
-# Assuming add_embeddings is a method of an instantiated class, use `instance.add_embeddings`
-# Replace `instance` with the actual instance of the class
-embed_function=IASOpenaiEmbeddings(engine='text-embedding-ada-002',client_id='dummy',total_token_embedding=[],request=fastapi_request)
-vector_db = IAS_OpenSearchVectorSearch(
-    index_name="reingest22",
-    embedding_function=embed_function,
-    opensearch_url=AWS_OPENSEARCH_HOST,
-    http_auth=(
-        AWS_OPENSEARCH_USERNAME,
-        AWS_OPENSEARCH_PASSWORD,
-    ),
-    is_aoss=False,
-)
-process_embeddings(vector_db.add_embeddings, text_embeddings, metadatas)
+{
+    "user_query": "subject: fwd: pp-e1a-usa-0200 - elra us app in-service deck.pdf
+from: shss-senior health and science specialist - pfizer
+date: wed, 27 sep 2023 12:11:55 -0400
+to: wai tak sung <wsung@mhs.net>
+mike nunez
+pfizer hematology institutional specialist
+florida
+c: 786.457.4168
+shss-senior health and science specialist - pfizer
+signatureimage
+begin forwarded message:
+from: hiriart, rita maria <shss-senior health and science specialist - pfizer>
+date: september 26, 2023 at 12:29:37 pm edt
+to: nunez, michael f <shss-senior health and science specialist - pfizer>
+subject: pp-e1a-usa-0200 - elra us app in-service deck.pdf
+edit, sign and share pdf files on the go. download acrobat reader for mobile:
+https://adobeacrobat.app.link/getapp
+fyi,
+rita hiriart
+pfizer hematology senior account specialist
+miami, fl & puerto rico
+cell: (786)287-7058
+sent from my ipad",
+    "metadata": {
+        "metadata": {
+            "category": "risk"
+        }
+    },
+    "context": [],
+    "temperature": 0,
+    "max_tokens": 12000,
+    "llm_engine": "gpt-35-turbo-16k",
+    "embedding_engine": "text-embedding-ada-002",
+    "llm_response_flag": false,
+    "num_of_citations": 5,
+    "with_score": true
+}
