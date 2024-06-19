5f18bff2a0165f14df44b25fcbc391fc2db1de57
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-19 21:44:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ class GraphBuilder:
         if self.config.get("entry_point") not in [node["name"] for node in self.config["nodes"]]:
             raise ValueError("Invalid entry point specified.")
 
-        if not any("END" in node.get("condition", {}).get("process", "") for node in self.config["nodes"]):
-            raise ValueError("No end state specified in the conditional edges.")
+        if not any("END" in process for node in self.config["nodes"] for process in node.get("condition", {}).get("process", {}).values()):
+            raise ValueError("No end state specified in any of the conditional edges.")
 
     def _load_config(self):
         nodes = {node["name"]: node for node in self.config["nodes"]}
@@ -51,3 +51,46 @@ class GraphBuilder:
             print(Exception)
             pass
         return app
+
+# Example usage
+config = {
+    "name": "Dummy Agent",
+    "type": "agent",
+    "prompt": "What is the meaning of life?",
+    "system_prompt": "Assume that you are a philosopher heavily influenced by Socrates and believe in equilibrium between liberal and capitalist ideas.",
+    "tools": [
+        { "name": "tool1", "method": "GET", "arn": "ASFGAGSDHDSHHDHH-adaHdh" },
+        { "name": "tool2", "method": "POST", "arn": "ASFGAGSDHDSHHDHH-adaHdh" },
+        { "name": "tool3", "method": "PUT", "arn": "ASFGAGSDHDSHHDHH-adaHdh" },
+        { "name": "tool4", "method": "GET", "arn": "ASFGAGSDHDSHHDHH-adaHdh" },
+        { "name": "tool5", "method": "GET", "arn": "ASFGAGSDHDSHHDHH-adaHdh" },
+        { "name": "rewrite", "method": "GET", "arn": "ASFGAGSDHDSHHDHH-adaHdh" }
+    ],
+    "model": "llama3",
+    "entry_point": "tool1",
+    "nodes": [
+        {
+            "name": "tool1",
+            "destination": "tool2"
+        },
+        {
+            "name": "tool2",
+            "destination": "tool3"
+        },
+        {
+            "name": "tool3",
+            "condition": {
+                "deciding_fn": "rewrite",
+                "process": "tool4",
+                "exit": "END"
+            }
+        },
+        {
+            "name": "tool4",
+            "destination": "tool1"
+        }
+    ]
+}
+
+graph_builder = GraphBuilder(config)
+app = graph_builder.compile()
