05b261fd30aff9da1fe4ef8dfcce1904eb19ebcf
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-06-19 21:27:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,38 @@
-from langgraph.graph import END, StateGraph
-
-workflow = StateGraph(GraphState)
-
-# Define the nodes
-workflow.add_node("retrieve", retrieve)  # retrieve
-workflow.add_node("grade_documents", grade_documents)  # grade documents
-workflow.add_node("generate", generate)  # generatae
-workflow.add_node("transform_query", transform_query)  # transform_query
-
-# Build graph
-workflow.set_entry_point("retrieve")
-workflow.add_edge("retrieve", "grade_documents")
-workflow.add_conditional_edges(
-    "grade_documents",
-    decide_to_generate,
-    {
-        "transform_query": "transform_query",
-        "generate": "generate",
-    },
-)
-workflow.add_edge("transform_query", "retrieve")
-workflow.add_conditional_edges(
-    "generate",
-    grade_generation_v_documents_and_question,
-    {
-        "not supported": "generate",
-        "useful": END,
-        "not useful": "transform_query",
-    },
-)
-
-# Compile
-app = workflow.compile()
-
-from IPython.display import Image, display
-
-try:
-    display(Image(app.get_graph(xray=True).draw_mermaid_png()))
-except Exception:
-    # This requires some extra dependencies and is optional
-    print(Exception)
-    pass
+{
+    "name": "Dummy Agent",
+    "type": "agent",
+    "prompt": "What is the meaning of life?",
+    "system_prompt": "Assume that you are a philosopher heavily influenced by Socrates and believe in equilibrium between liberal and capitalist ideas.",
+    "tools": [
+        { "name": "tool1", "method": "GET", "arn": "ASFGAGSDHDSHHDHH-adaHdh" },
+        { "name": "tool2", "method": "POST", "arn": "ASFGAGSDHDSHHDHH-adaHdh" },
+        { "name": "tool3", "method": "PUT", "arn": "ASFGAGSDHDSHHDHH-adaHdh" },
+        { "name": "tool4", "method": "GET", "arn": "ASFGAGSDHDSHHDHH-adaHdh" },
+        { "name": "tool5", "method": "GET", "arn": "ASFGAGSDHDSHHDHH-adaHdh" },
+        { "name": "rewrite", "method": "GET", "arn": "ASFGAGSDHDSHHDHH-adaHdh" }
+    ],
+    "model": "llama3",
+    "entry_point": "tool1",
+    "nodes": [
+        {
+            "name": "tool1",
+            "destination": "tool2"
+        },
+        {
+            "name": "tool2",
+            "destination": "tool3"
+        },
+        {
+            "name": "tool3",
+            "condition": {
+                "deciding_fn": "rewrite",
+                "process": "tool4",
+                "exit": "END"
+            }
+        },
+        {
+            "name": "tool4",
+            "destination": "tool1"
+        }
+    ]
+}
