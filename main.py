666c30f9d02f9ab329ee1b040f07d1e0fe9e1b1a
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-07-03 23:17:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
     def extract_sections_from_toc(self):
-        document = Document(self.docx_path)
-        for para in document.paragraphs:
-            if para.style.name.startswith('Heading'):
-                normalized_title = para.text.strip()
-                if normalized_title not in self.sections:
-                    self.sections.append(normalized_title)
-                    self.section_contents[normalized_title] = []
+        try:
+            with zipfile.ZipFile(self.docx_path) as docx:
+                xml_content = docx.read('word/document.xml')
+            tree = etree.XML(xml_content)
+ 
+            toc_started = False
+ 
+            for elem in tree.iter():
+                if 'fldSimple' in elem.tag or 'instrText' in elem.tag:
+                    #print(elem.tag)
+                    if 'TOC' in ''.join(elem.itertext()):
+                        print("===================elem text",elem.itertext())
+                        print("===================something",''.join(elem.itertext()))
+                        toc_started = True
+                        continue
+                if toc_started and elem.tag.endswith('}hyperlink'):
+                    section_title = ''.join([e for e in elem.itertext()]).strip()
+                    if section_title and section_title not in self.sections:
+                        self.sections.append(section_title)
+                        self.section_contents[section_title] = []
+        except Exception as e:
+            print(f"Failed in extract_sections_from_toc with error: {e}")
