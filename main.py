994fc6ab132dbb4450d3221ea19573682d62c714
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-07-09 19:17:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,20 @@
-# Define custom function directory
-ARG FUNCTION_DIR="/function"
-
-FROM 420737321821.dkr.ecr.us-east-1.amazonaws.com/core-vsl-python3.11-slim as build-image
-
-# Include global arg in this stage of the build
-ARG FUNCTION_DIR
-
-# Update package lists and install required packages including git
-RUN apt-get update \
-    && apt-get install -y \
-        git \
-    && rm -rf /var/lib/apt/lists/*
-
-# Copy function code
-RUN mkdir -p ${FUNCTION_DIR}
-COPY . ${FUNCTION_DIR}
-
-# Set working directory to function root directory
-WORKDIR ${FUNCTION_DIR}
-
-# Install the function's dependencies
-RUN pip3 install --target ${FUNCTION_DIR} -r requirements.txt
-RUN pip3 install --target ${FUNCTION_DIR} awslambdaric
-
-FROM 420737321821.dkr.ecr.us-east-1.amazonaws.com/core-vsl-python3.11-slim
-
-# Update package lists and install required packages
-RUN apt-get update \
-    && apt-get install -y \
-        rpm \
-        curl \
-        libgl1 \
-        poppler-utils \  
-        libmagic-dev \
-        libreoffice \
-        git \
-    && rm -rf /var/lib/apt/lists/*
-
-# Include global arg in this stage of the build
-ARG FUNCTION_DIR
-
-# Set working directory to function root directory
-WORKDIR ${FUNCTION_DIR}
-
-ARG GITHUB_TOKEN
-
-# Set the environment variable GITHUB_TOKEN to the value of the build argument
-ENV GITHUB_TOKEN ${GITHUB_TOKEN}
-
-# Configure git with the GITHUB_TOKEN for the specific GitHub repository:
-# This command will only replace "https://github.com/pfizer/vessel-services" with "https://${GITHUB_TOKEN}:x-oauth-basic@github.com/pfizer/vessel-services"
-# so that Git operations on the specific pfizer repository will use the provided token for authentication.
-RUN git config --global url."https://${GITHUB_TOKEN}:x-oauth-basic@github.com/pfizer/vessel-services".insteadOf "https://github.com/pfizer/vessel-services"
-
-# Copy in the built dependencies
-COPY --from=build-image ${FUNCTION_DIR} ${FUNCTION_DIR}
-
-# Set runtime interface client as default command for the container runtime
-ENTRYPOINT [ "/usr/local/bin/python", "-m", "awslambdaric" ]
-
-# Pass the name of the function handler as an argument to the runtime
-CMD [ "app.lambda_handler" ]
+#12 [build-image 6/7] RUN  pip3 install --target /function -r requirements.txt
+1469#12 1.037 Collecting vsl_utils@ git+https://github.com/pfizer/vessel-services.git@dev#subdirectory=utils/vsl_utils (from -r requirements.txt (line 16))
+1470#12 1.039   Cloning https://github.com/pfizer/vessel-services.git (to revision dev) to /tmp/pip-install-6tcgmvzn/vsl-utils_df556f7eb6ed4ec595400fdc942d15f5
+1471#12 1.043   Running command git clone --filter=blob:none --quiet https://github.com/pfizer/vessel-services.git /tmp/pip-install-6tcgmvzn/vsl-utils_df556f7eb6ed4ec595400fdc942d15f5
+1472#12 1.190   fatal: could not read Username for 'https://github.com': No such device or address
+1473#12 1.203   error: subprocess-exited-with-error
+1474#12 1.203   
+1475#12 1.203   × git clone --filter=blob:none --quiet https://github.com/pfizer/vessel-services.git /tmp/pip-install-6tcgmvzn/vsl-utils_df556f7eb6ed4ec595400fdc942d15f5 did not run successfully.
+1476#12 1.203   │ exit code: 128
+1477#12 1.203   ╰─> See above for output.
+1478#12 1.203   
+1479#12 1.203   note: This error originates from a subprocess, and is likely not a problem with pip.
+1480#12 1.206 error: subprocess-exited-with-error
+1481#12 1.206
+1482#12 1.206 × git clone --filter=blob:none --quiet https://github.com/pfizer/vessel-services.git /tmp/pip-install-6tcgmvzn/vsl-utils_df556f7eb6ed4ec595400fdc942d15f5 did not run successfully.
+1483#12 1.206 │ exit code: 128
+1484#12 1.206 ╰─> See above for output.
+1485#12 1.206
+1486#12 1.206 note: This error originates from a subprocess, and is likely not a problem with pip.
+1487#12 1.445
