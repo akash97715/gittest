fa493aef230cf974c4ec064e698f2241146ab0b2
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-07-12 13:48:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,1 @@
-UPDATE documents
-SET is_file_deleted = TRUE
-WHERE index_name = 'biopharma' AND client_name = 'bpm';
+list_tobesorted.sort(key=lambda x: (x['metadata']['docinsight_custom_extraction_type'],x['metadata']['figure_name'], x['metadata']['page']))
