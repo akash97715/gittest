214c4761d4384d4729254cb1de1a9cfc5b56e458
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-07-17 11:10:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
-AttributeError                            Traceback (most recent call last) Cell In[14], line 1----> 1 tree=Workflow(config=config) Cell In[12], line 21, in Workflow.__init__(self, config)     19 self._validate_config()     20 self._create_lambda_wrappers()---> 21 self._load_config() Cell In[12], line 61, in Workflow._load_config(self)     59     print(node)     60     action = self.lambda_wrappers.get(node["name"])---> 61     self.workflow.add_node(node["name"], action.run)     63 self.workflow.set_entry_point(self.config["entry_point"])     65 for node in self.config["nodes"]:AttributeError: 'NoneType' object has no attribute 'run'
+import json
+import boto3
+from langchain_core.pydantic_v1 import BaseModel, ValidationError
+from pydantic import validate_call
+from typing import Any, Optional, Dict
+
+class LambdaWrapper(BaseModel):
+    lambda_client: Any
+    function_name: Optional[str] = None
+    awslambda_tool_name: Optional[str] = None
+    awslambda_tool_description: Optional[str] = None
+    is_conditional_fn: bool = False
+    input_schema: BaseModel = None
+    output_schema: BaseModel = None
+ 
+    @classmethod
+    def create(cls, function_name: str, tool_name: str, tool_description: str, region: str, input_schema: BaseModel, output_schema: BaseModel, is_conditional: bool = False):
+        lambda_client = boto3.client('lambda', region_name=region)
+        return cls(
+            lambda_client=lambda_client,
+            function_name=function_name,
+            awslambda_tool_name=tool_name,
+            awslambda_tool_description=tool_description,
+            input_schema=input_schema,
+            output_schema=output_schema,
+            is_conditional_fn=is_conditional
+        )
+ 
+    @validate_call
+    def validate_input(self, input_data: Dict):
+        if self.input_schema:
+            self.input_schema(**input_data)
+
+    @validate_call
+    def validate_output(self, output_data: Dict):
+        if self.output_schema:
+            self.output_schema(**output_data)
+
+    def run(self, state: Dict) -> Dict:
+        self.validate_input(state)
+        response = self.lambda_client.invoke(
+            FunctionName=self.function_name,
+            InvocationType='RequestResponse',
+            Payload=json.dumps({"body": state})
+        )
+        payload_string = response['Payload'].read().decode('utf-8')
+        output_data = json.loads(payload_string)
+        self.validate_output(output_data)
+        return output_data
