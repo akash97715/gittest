7522fcbb55c0c307c1d0835a2332038f90e6ea05
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-07-17 11:05:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-    def _create_lambda_wrappers(self):
-        for tool in self.config["tools"]:
-            input_schema = tool.get("input_schema")  # Assuming input_schema is provided correctly
-            output_schema = tool.get("output_schema")  # Assuming output_schema is provided correctly
-            wrapper = LambdaWrapper.create(
-                function_name=tool["arn"],
-                tool_name=tool["name"],
-                tool_description=f"Lambda function for {tool['name']}",
-                region=tool["region"],
-                input_schema=input_schema,
-                output_schema=output_schema
-            )
-            self.lambda_wrappers[tool["name"]] = wrapper
+AttributeError                            Traceback (most recent call last) Cell In[14], line 1----> 1 tree=Workflow(config=config) Cell In[12], line 21, in Workflow.__init__(self, config)     19 self._validate_config()     20 self._create_lambda_wrappers()---> 21 self._load_config() Cell In[12], line 61, in Workflow._load_config(self)     59     print(node)     60     action = self.lambda_wrappers.get(node["name"])---> 61     self.workflow.add_node(node["name"], action.run)     63 self.workflow.set_entry_point(self.config["entry_point"])     65 for node in self.config["nodes"]:AttributeError: 'NoneType' object has no attribute 'run'
