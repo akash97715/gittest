1f58e721b63a2b7305329e572f7ffc5270392ab8
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-07-21 23:50:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,68 @@
----------------------------------------------------------------------------GenericException                          Traceback (most recent call last) File d:\docinsightsync\vessel-services\agenttemp\utils\ias_openai_langchain.py:380, in ias_openai_chat_completion_with_tools(engine, temperature, max_tokens, client_id, x_vsl_client_id, bearer_token, messages, tools, tool_choice)    377     logger.error(    378         f"Error calling OpenAI chat completion API: {response.status_code}, {response.json()}"    379     )--> 380     raise GenericException(    381         f"Error calling OpenAI chat completion API: {response.status_code}, {response.json()}",    382         status_code=response.status_code,    383     )    384 chat_completion = json.loads(response.json()["result"])GenericException: Error calling OpenAI chat completion API: 400, {'detail': 'Error chatCompletion: {\n  "error": {\n    "message": "Invalid value for \'tool_choice\': \'tool_choice\' is only allowed when \'tools\' are specified.",\n    "type": "invalid_request_error",\n    "param": "tool_choice",\n    "code": null\n  }\n}\n'} During handling of the above exception, another exception occurred:GenericException                          Traceback (most recent call last) Cell In[9], line 2      1 inputs = {"input": "what is the weather in india", "chat_history": []}----> 2 app.invoke(inputs) File d:\docinsight_langgraph\docinsightlanggraph\Lib\site-packages\langgraph\pregel\__init__.py:1668, in Pregel.invoke(self, input, config, stream_mode, output_keys, input_keys, interrupt_before, interrupt_after, debug, **kwargs) 1666 else: 1667 chunks = []-> 1668 for chunk in self.stream(
-...
-    389 logger.error("Got the Exception", str(e))    390 # raising backoff exception--> 391 raise GenericException(e) GenericException: Error calling OpenAI chat completion API: 400, {'detail': 'Error chatCompletion: {\n  "error": {\n    "message": "Invalid value for \'tool_choice\': \'tool_choice\' is only allowed when \'tools\' are specified.",\n    "type": "invalid_request_error",\n    "param": "tool_choice",\n    "code": null\n  }\n}\n'}
+@backoff.on_exception(
+    backoff.expo, GenericException, max_tries=20, max_time=60, giveup=is_http_4xx_error
+)
+def ias_openai_chat_completion_with_tools(
+    engine: str,
+    temperature: float,
+    max_tokens: int,
+    client_id: str = None,
+    x_vsl_client_id: str = None,
+    bearer_token: str = None,
+    messages: List[BaseMessage] = None,
+    tools: List[BaseTool] = None,
+    tool_choice: str = None,
+) -> str:
+    """
+    Generates a chat completion response for OpenAI model
+    :param token: auth token
+    :param user_message: user's prompt
+    :param engine: model capable for chat completion i.e. gpt*
+    :param temperature: value 0-1 that tells model to be more precise or generative
+    :param max_tokens: max tokens the prompt & response should be. It depends on the model's capacity
+    :return: response from OpenAI model
+    """
+    try:
+        payload = {
+            "engine": engine,
+            "messages": messages,
+            "temperature": temperature,
+            "max_tokens": max_tokens,
+            "tools": tools,
+            "tool_choice": tool_choice,
+        }
+ 
+        token = get_auth_token(bearer_token)
+ 
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {token}",
+        }
+        if x_vsl_client_id is not None:
+            headers["x-vsl-client_id"] = x_vsl_client_id
+        elif client_id is not None:
+            headers["x-vsl-client_id"] = client_id
+ 
+        logger.info("Calling chat completion endpoint with tools")
+        logger.info(payload)
+ 
+        response = requests.post(IAS_OPENAI_CHAT_URL, headers=headers, json=payload)
+ 
+        logger.info("Received response from llm")
+        logger.info(response.json())
+ 
+        if response.status_code != 200:
+            logger.error(
+                f"Error calling OpenAI chat completion API: {response.status_code}, {response.json()}"
+            )
+            raise GenericException(
+                f"Error calling OpenAI chat completion API: {response.status_code}, {response.json()}",
+                status_code=response.status_code,
+            )
+        chat_completion = json.loads(response.json()["result"])
+ 
+        total_token_completion = int(response.json()["totalTokens"])
+        return chat_completion, total_token_completion
+    except Exception as e:
+        logger.error("Got the Exception", str(e))
+        # raising backoff exception
+        raise GenericException(e)
