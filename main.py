cf4b1c919d368f484676a1bb6eee574002ecd659
akash97715 <34799343+akash97715@users.noreply.github.com>
2024-07-22 21:22:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,105 +1,62 @@
-[9:08 PM] Deep, Akash (External)
 import operator
-from typing import Annotated, TypedDict, Union
- 
+from typing import Annotated, List, TypedDict, Union
+
 from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.messages import BaseMessage
- 
- 
-class AgentState(TypedDict):
-    # The input string
-    input: str
-    # The list of previous messages in the conversation
-    chat_history: list[BaseMessage]
-    # The outcome of a given call to the agent
-    # Needs `None` as a valid type, since this is what this will start as
-    agent_outcome: Union[AgentAction, AgentFinish, None]
-    # List of actions and corresponding observations
-    # Here we annotate this with `operator.add` to indicate that operations to
-    # this state should be ADDED to the existing values (not overwrite it)
-    intermediate_steps: Annotated[list[tuple[AgentAction, str]], operator.add]
- 
-[9:08 PM] Deep, Akash (External)
-from langchain_core.agents import AgentFinish
- 
-from langgraph.prebuilt.tool_executor import ToolExecutor
- 
-# This a helper class we have that is useful for running tools
-# It takes in an agent action and calls that tool and returns the result
-tool_executor = ToolExecutor(tools)
- 
- 
-# Define the agent
-def run_agent(data):
-    agent_outcome = agent_runnable.invoke(data)
-    return {"agent_outcome": agent_outcome}
- 
- 
-# Define the function to execute tools
-def execute_tools(data):
-    # Get the most recent agent_outcome - this is the key added in the `agent` above
-    agent_action = data["agent_outcome"]
-    output = tool_executor.invoke(agent_action)
-    return {"intermediate_steps": [(agent_action, str(output))]}
- 
- 
-# Define logic that will be used to determine which conditional edge to go down
-def should_continue(data):
-    # If the agent outcome is an AgentFinish, then we return `exit` string
-    # This will be used when setting up the graph to define the flow
-    if isinstance(data["agent_outcome"], AgentFinish):
-        return "end"
-    # Otherwise, an AgentAction is returned
-    # Here we return `continue` string
-    # This will be used when setting up the graph to define the flow
-    else:
-        return "continue"
- 
-[9:08 PM] Deep, Akash (External)
+from langchain.prebuilt.tool_executor import ToolExecutor
 from langgraph.graph import END, StateGraph, START
- 
-# Define a new graph
-workflow = StateGraph(AgentState)
- 
-# Define the two nodes we will cycle between
-workflow.add_node("agent", run_agent)
-workflow.add_node("action", execute_tools)
- 
-# Set the entrypoint as `agent`
-# This means that this node is the first one called
-workflow.add_edge(START, "agent")
- 
-# We now add a conditional edge
-workflow.add_conditional_edges(
-    # First, we define the start node. We use `agent`.
-    # This means these are the edges taken after the `agent` node is called.
-    "agent",
-    # Next, we pass in the function that will determine which node is called next.
-    should_continue,
-    # Finally we pass in a mapping.
-    # The keys are strings, and the values are other nodes.
-    # END is a special node marking that the graph should finish.
-    # What will happen is we will call `should_continue`, and then the output of that
-    # will be matched against the keys in this mapping.
-    # Based on which one it matches, that node will then be called.
-    {
-        # If `tools`, then we call the tool node.
-        "continue": "action",
-        # Otherwise we finish.
-        "end": END,
-    },
-)
- 
-# We now add a normal edge from `tools` to `agent`.
-# This means that after `tools` is called, `agent` node is called next.
-workflow.add_edge("action", "agent")
- 
-# Finally, we compile it!
-# This compiles it into a LangChain Runnable,
-# meaning you can use it as you would any other runnable
-app = workflow.compile()
- 
-[9:09 PM] Deep, Akash (External)
-inputs = {"input": "get the weather in india and then send an email. use lambda email tool", "chat_history": []}
-app.invoke(inputs)
- 
+
+class AgentState(TypedDict):
+    input: str
+    chat_history: List[BaseMessage]
+    agent_outcome: Union[AgentAction, AgentFinish, None]
+    intermediate_steps: Annotated[List[tuple[AgentAction, str]], operator.add]
+
+class WorkflowManager:
+    def __init__(self, config):
+        self.config = config
+        self.tool_executor = ToolExecutor(self.config['tools'])
+        self.workflow = self.setup_workflow()
+
+    def setup_workflow(self):
+        workflow = StateGraph(AgentState)
+        workflow.add_node("agent", self.run_agent)
+        workflow.add_node("action", self.execute_tools)
+        workflow.add_edge(START, "agent")
+        workflow.add_conditional_edges(
+            "agent",
+            self.should_continue,
+            {
+                "continue": "action",
+                "end": END,
+            }
+        )
+        workflow.add_edge("action", "agent")
+        return workflow.compile()
+
+    def run_agent(self, data):
+        agent_outcome = self.config['agent_runnable'].invoke(data)
+        return {"agent_outcome": agent_outcome}
+
+    def execute_tools(self, data):
+        agent_action = data["agent_outcome"]
+        output = self.tool_executor.invoke(agent_action)
+        return {"intermediate_steps": [(agent_action, str(output))]}
+
+    def should_continue(self, data):
+        if isinstance(data["agent_outcome"], AgentFinish):
+            return "end"
+        else:
+            return "continue"
+
+    def run(self, query):
+        inputs = {"input": query, "chat_history": []}
+        return self.workflow.invoke(inputs)
+
+# Example usage:
+config = {
+    'tools': [],  # Replace with actual tools configuration
+    'agent_runnable': lambda data: AgentAction()  # Replace with actual runnable logic
+}
+manager = WorkflowManager(config)
+result = manager.run("get the weather in India and then send an email. Use Lambda email tool")
